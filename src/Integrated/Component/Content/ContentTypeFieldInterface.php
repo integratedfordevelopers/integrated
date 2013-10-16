51b13ab4e05abfa736b8b3d40b5d0670db24def7
Jeroen van Leeuwen <jeroen@e-active.nl>
2013-10-16 13:24:57 +0200

    Interfaces added

---
+++ src/Integrated/Component/Content/ContentTypeFieldInterface.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* This file is part of the Integrated package.
+*
+* (c) e-Active B.V. <integrated@e-active.nl>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Integrated\Component\Content;
+
+/**
+ * @author Jeroen van Leeuwen <jeroen@e-active.nl>
+ * @author Jan Sanne Mulder <jansanne@e-active.nl>
+ */
+interface ContentTypeFieldInterface
+{
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * @return string
+     */
+    public function getLabel();
+}
