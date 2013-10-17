022051a201d016130f3c3c23bd3db24070ad158d
Jan Sanne <j.san.mulder@gmail.com>
2013-10-17 10:51:12 +0200

    fixed type in licence information

--- src/Integrated/Component/Content/Mapping/Annotations/Document.php
+++ src/Integrated/Component/Content/Mapping/Annotations/Document.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Integrated package.
  *
- * (c) e-Active BV <integrated@e-active.nl>
+ * (c) e-Active B.V. <integrated@e-active.nl>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
