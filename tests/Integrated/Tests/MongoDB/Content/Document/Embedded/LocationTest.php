eca6fa2fe063c3c7747c6379c29c99dbe7634262
Jeroen van Leeuwen <jeroen@e-active.nl>
2013-10-31 15:31:06 +0100

    Added test for Document\Embedded\Location

---
+++ tests/Integrated/Tests/MongoDB/Content/Document/Embedded/LocationTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Tests\MongoDB\Content\Document\Embedded;
+
+use Integrated\MongoDB\Content\Document\Embedded\Location;
+
+/**
+ * @author Jeroen van Leeuwen <jeroen@e-active.nl>
+ */
+class LocationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Location
+     */
+    private $location;
+
+    /**
+     * Setup the test
+     */
+    protected function setUp()
+    {
+        $this->location = new Location();
+    }
+
+    /**
+     * Test get- and setLatitude function
+     */
+    public function testGetAndSetLatitudeFunction()
+    {
+        $latitude = 1.5;
+        $this->assertEquals($latitude, $this->location->setLatitude($latitude)->getLatitude());
+    }
+
+    /**
+     * Test get- and setLongitude function
+     */
+    public function testGetAndSetLongitudeFunction()
+    {
+        $longitude = 5.5;
+        $this->assertEquals($longitude, $this->location->setLongitude($longitude)->getLongitude());
+    }
+}
