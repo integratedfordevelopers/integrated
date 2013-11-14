4a54aef1b69957dd2279aff095c28b66f0087b04
Jan Sanne <j.san.mulder@gmail.com>
2013-11-14 13:22:19 +0100

    Added unit test classes (still needs actual tests) for the ClassMetadataFactory and its dependencies.

---
+++ tests/Integrated/Tests/MongoDB/ContentType/DiscriminatorMapBuilderSubscriberTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Tests\MongoDB\ContentType;
+
+/**
+ * @author Jan Sanne Mulder <jansanne@e-active.nl>
+ */
+class DiscriminatorMapBuilderSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+
+}
+ 
