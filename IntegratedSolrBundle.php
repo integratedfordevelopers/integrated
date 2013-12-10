77e573a527d5ec827260a1cf8c30010f268d98e0
Jeroen van Leeuwen <jeroen@e-active.nl>
2013-12-10 16:35:06 +0100

    Added getContainerExtension and PHPDoc

--- IntegratedSolrBundle.php
+++ IntegratedSolrBundle.php
@@ -1,9 +1,25 @@
 <?php
 
+/*
+* This file is part of the Integrated package.
+*
+* (c) e-Active B.V. <integrated@e-active.nl>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Integrated\Bundle\SolrBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IntegratedSolrBundle extends Bundle
 {
+    /**
+     * @inheritdoc
+     */
+    public function getContainerExtension()
+    {
+        return new DependencyInjection\IntegratedSolrExtension();
+    }
 }
