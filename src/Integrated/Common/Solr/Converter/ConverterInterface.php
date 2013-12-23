35723b9e36e80d32f55b1546d450ef13b1194866
Jeroen van Leeuwen <jeroen@e-active.nl>
2013-12-23 14:16:36 +0100

    Added interface for converter

---
+++ src/Integrated/Common/Solr/Converter/ConverterInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Common\Solr\Converter;
+
+/**
+ * @author Jeroen van Leeuwen <jeroen@e-active.nl>
+ */
+interface ConverterInterface
+{
+    /**
+     * @param object $object
+     */
+    public function getDocument($object);
+}
