19e6d7eeaeefd9602c66cecc948bee6341586b28
Jeroen van Leeuwen <jeroen@e-active.nl>
2014-01-02 13:03:28 +0100

    Fixed typo in setGender

--- Document/Content/Relation/Person.php
+++ Document/Content/Relation/Person.php
@@ -86,12 +86,12 @@ class Person extends Relation
     /**
      * Set the gender of the document
      *
-     * @param string $sex
+     * @param string $gender
      * @return $this
      */
-    public function setGender($sex)
+    public function setGender($gender)
     {
-        $this->gender = $sex;
+        $this->gender = $gender;
         return $this;
     }
 
