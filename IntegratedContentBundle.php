a91c392036dcc97f0ff143150195e4dc83e322e8
Jan Sanne <j.san.mulder@gmail.com>
2014-03-20 14:31:08 +0100

    Extensions start

--- IntegratedContentBundle.php
+++ IntegratedContentBundle.php
@@ -3,6 +3,7 @@
 namespace Integrated\Bundle\ContentBundle;
 
 use Integrated\Bundle\ContentBundle\DependencyInjection\Compiler\DoctrineMongoDBMetadataFactoryPass;
+use Integrated\Bundle\ContentBundle\DependencyInjection\Compiler\ExtensionRegistryBuilderPass;
 use Integrated\Bundle\ContentBundle\DependencyInjection\IntegratedContentExtension;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -15,7 +16,10 @@ class IntegratedContentBundle extends Bundle
 	 */
 	public function build(ContainerBuilder $container)
 	{
+		parent::build($container);
+
 		$container->addCompilerPass(new DoctrineMongoDBMetadataFactoryPass());
+		$container->addCompilerPass(new ExtensionRegistryBuilderPass());
 	}
 
 	/**
