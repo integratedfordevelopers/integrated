23743bba29237a92f62bb3f55595dac1bd024364
Jeroen van Leeuwen <jeroen@e-active.nl>
2014-04-16 16:21:19 +0200

    Added RelationType form in ContentBundle instead of library

---
+++ Form/DataTransformer/Relations.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\ContentBundle\Form\DataTransformer;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Util\Debug;
+use Integrated\Bundle\ContentBundle\Document\Content\Content;
+use Symfony\Component\Form\DataTransformerInterface;
+use Integrated\Common\ContentType\Mapping\Metadata;
+use Integrated\Common\ContentType\ContentTypeRelationInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Integrated\Bundle\ContentBundle\Document\Content\Embedded\Relation as Model;
+
+/**
+ * @author Jeroen van Leeuwen <jeroen@e-active.nl>
+ */
+class Relations implements DataTransformerInterface
+{
+    /**
+     * @var string
+     */
+    const REPOSITORY = 'Integrated\Bundle\ContentBundle\Document\Content\Content';
+
+    /**
+     * @var ContentTypeRelationInterface[]
+     */
+    protected $relations;
+
+    /**
+     * @var ObjectManager
+     */
+    protected $om;
+
+    /**
+     * @param ContentTypeRelationInterface[] $relations
+     * @param ObjectManager $om
+     */
+    public function __construct($relations, $om)
+    {
+        $this->relations = $relations;
+        $this->om = $om;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value)
+    {
+        $return = array();
+        if (is_array($value) || $value instanceof \Traversable) {
+
+
+
+            foreach ($value as $relation) {
+
+
+                //echo $relation->getContentTypeRelation() .  '<br />';
+                if ($relation instanceof \Integrated\Bundle\ContentBundle\Document\Content\Embedded\Relation) {
+
+
+                    foreach ($this->relations as $contentTypeRelation) {
+                        if ($contentTypeRelation->getId() == $relation->getContentTypeRelation()) {
+
+                            $references = array();
+                            foreach ($relation->getReferences() as $content) {
+                                $references[] = $content->getId();
+                            }
+
+                            $return[$relation->getContentTypeRelation()] = implode(',', $references);
+                        }
+                    }
+                }
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform($value)
+    {
+        $relations = new ArrayCollection();
+
+        if (is_array($value)) {
+            foreach ($value as $contentTypeRelation => $references) {
+
+                $model = new Model();
+                $model->setContentTypeRelation($contentTypeRelation);
+
+                if (null !== $references) {
+                    $references = array_filter(explode(',', $references));
+                    foreach ($references as $reference) {
+
+                        if ($content = $this->om->getRepository(self::REPOSITORY)->find($reference)) {
+                            $model->addReference($content);
+                        }
+                    }
+
+                    $relations->add($model);
+                }
+            }
+        }
+
+        return $relations;
+    }
+}
