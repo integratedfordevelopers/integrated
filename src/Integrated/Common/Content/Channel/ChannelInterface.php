00c11085aefd8f65676f606db583c5b5b98afbef
Jan Sanne <jansanne@e-active.nl>
2014-09-09 12:04:16 +0200

    Updated interface with new methods

--- src/Integrated/Common/Content/Channel/ChannelInterface.php
+++ src/Integrated/Common/Content/Channel/ChannelInterface.php
@@ -18,10 +18,24 @@ namespace Integrated\Common\Content\Channel;
  */
 interface ChannelInterface
 {
-    /**
-     * Return the id of the Channel document
-     *
-     * @return string
-     */
-    public function getId();
+	/**
+	 * Return the id of the Channel document
+	 *
+	 * @return string
+	 */
+	public function getId();
+
+	/**
+	 * Return the name of the Channel document
+	 *
+	 * @return string
+	 */
+	public function getName();
+
+	/**
+	 * Return the short name of the Channel document
+	 *
+	 * @return string
+	 */
+	public function getShortName();
 }
