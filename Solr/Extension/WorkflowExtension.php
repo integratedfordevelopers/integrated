8c396613d48311efec7f88c9aa08256ff3d1ce53
Jan Sanne <jansanne@e-active.nl>
2014-10-23 18:04:32 +0200

    Made changes to solr indexing to use new converter.

---
+++ Solr/Extension/WorkflowExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\WorkflowBundle\Solr\Extension;
+
+use Integrated\Common\Converter\ContainerInterface;
+use Integrated\Common\Converter\Type\TypeExtensionInterface;
+
+/**
+ * @author Jan Sanne Mulder <jansanne@e-active.nl>
+ */
+class WorkflowExtension implements TypeExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerInterface $container, $data, array $options = [])
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'integrated.content';
+    }
+}
