5bb0364bd1537d9b80b567d246185a089171e351
Jan Sanne <jansanne@e-active.nl>
2014-10-23 18:31:45 +0200

    Started in workflow solr extension.

--- Solr/Extension/WorkflowExtension.php
+++ Solr/Extension/WorkflowExtension.php
@@ -11,8 +11,14 @@
 
 namespace Integrated\Bundle\WorkflowBundle\Solr\Extension;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use Integrated\Bundle\WorkflowBundle\Entity\Workflow\State;
+use Integrated\Common\Content\ContentInterface;
+
+use Integrated\Common\ContentType\Resolver\ContentTypeResolverInterface;
 use Integrated\Common\Converter\ContainerInterface;
 use Integrated\Common\Converter\Type\TypeExtensionInterface;
+use Symfony\Component\Security\Core\Util\ClassUtils;
 
 /**
  * @author Jan Sanne Mulder <jansanne@e-active.nl>
@@ -20,10 +26,32 @@ use Integrated\Common\Converter\Type\TypeExtensionInterface;
 class WorkflowExtension implements TypeExtensionInterface
 {
     /**
+   	 * @var \Symfony\Component\DependencyInjection\ContainerInterface
+   	 */
+   	private $container;
+
+   	/**
+   	 * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   	 */
+   	public function __construct(\Symfony\Component\DependencyInjection\ContainerInterface $container)
+   	{
+   		$this->container = $container;
+   	}
+
+    /**
      * {@inheritdoc}
      */
     public function build(ContainerInterface $container, $data, array $options = [])
     {
+        if (!$data instanceof ContentInterface) {
+            return; // only process content
+        }
+
+        if (!$state = $this->resolveState($data)) {
+            return; // got not workflow
+        }
+
+        $container;
     }
 
     /**
@@ -33,4 +61,54 @@ class WorkflowExtension implements TypeExtensionInterface
     {
         return 'integrated.content';
     }
+
+    /**
+   	 * @return ObjectManager
+   	 */
+   	protected function getManager()
+   	{
+   		return $this->container->get('integrated_workflow.extension.doctrine.object_manager');
+   	}
+
+   	/**
+   	 * @return ContentTypeResolverInterface
+   	 */
+   	protected function getResolver()
+   	{
+        return $this->container->get('integrated.form.resolver');
+   	}
+
+    /**
+   	 * @param ContentInterface $content
+     *
+   	 * @return null | State
+   	 */
+   	protected function resolveState(ContentInterface $content)
+   	{
+		// does this content even have a workflow connected ?
+
+		if (!$type = $this->getResolver()->getType(ClassUtils::getRealClass($content), $content->getContentType())) {
+            return null;
+		}
+
+        if (!$type->getOption('workflow')) {
+            return null;
+        }
+
+   		$repository = $this->getManager()->getRepository('Integrated\\Bundle\\WorkflowBundle\\Entity\\Workflow\\State');
+
+   		if ($entity = $repository->findOneBy(['content' => $content])) {
+   			return $entity;
+   		}
+
+        // get default workflow
+
+        $repository = $this->getManager()->getRepository('Integrated\\Bundle\\WorkflowBundle\\Entity\\Definition');
+
+        if ($entity = $repository->find($type->getOption('workflow'))) {
+            return $entity->getDefault();
+        }
+
+   		return null;
+   	}
 }
