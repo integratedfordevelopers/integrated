14500bdd26831c7c587d1164fc1727d71daa15e9
Ger Jan <gerjan@e-active.nl>
2014-11-18 13:07:26 +0100

    Initial commit

---
+++ IntegratedWebsiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Integrated\Bundle\WebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IntegratedWebsiteBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# IntegratedWebsiteBundle
+
+TODO
