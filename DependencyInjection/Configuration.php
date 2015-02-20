db55a057893b3571d2b572741dd4af3b128b5100
Ger Jan <gerjan@e-active.nl>
2015-02-20 10:56:42 +0100

    Initial commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\SlugBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Ger Jan van den Bosch <gerjan@e-active.nl>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('integrated_slug');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# IntegratedSlugBundle
+
+## Installation
+
+composer.json
+
+    "integrated/slug-bundle": "dev-master",
+
+AppKernel.php
+
+    $bundles = array(
+        ...
+        new Integrated\Bundle\SlugBundle\IntegratedSlugBundle(),
+        ...
+    );
