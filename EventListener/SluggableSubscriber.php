510feaef4e375d90a28a241f0825bdad927479f4
Ger Jan <gerjan@e-active.nl>
2015-03-04 16:46:27 +0100

    Added auto-increment to slug Subscriber

--- EventListener/SluggableSubscriber.php
+++ EventListener/SluggableSubscriber.php
@@ -11,18 +11,23 @@
 
 namespace Integrated\Bundle\SlugBundle\EventListener;
 
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
 use Doctrine\Common\EventSubscriber;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\Common\Persistence\Mapping\MappingException;
 use Doctrine\Common\Persistence\ObjectManager;
 
+use Doctrine\ODM\MongoDB\Event\PreUpdateEventArgs as ODMPreUpdateEventArgs;
+use Doctrine\ODM\MongoDB\UnitOfWork as ODMUnitOfWork;
 use Doctrine\ODM\MongoDB\DocumentManager;
+
+use Doctrine\ORM\Event\PreUpdateEventArgs as ORMPreUpdateEventArgs;
+use Doctrine\ORM\UnitOfWork as ORMUnitOfWork;
 use Doctrine\ORM\EntityManager;
 
-use Doctrine\ODM\MongoDB\UnitOfWork as ODMUnitOfWork;
-use Doctrine\ORM\UnitOfWork as ORMUnitOfWork;
-
 use Metadata\MetadataFactoryInterface;
+use Metadata\MergeableClassMetadata;
 
 use Integrated\Bundle\SlugBundle\Mapping\Metadata\PropertyMetadata;
 use Integrated\Bundle\SlugBundle\Slugger\SluggerInterface;
@@ -83,8 +88,6 @@ class SluggableSubscriber implements EventSubscriber
 
     /**
      * @param LifecycleEventArgs $args
-     *
-     * @throws MappingException
      */
     protected function handleEvent(LifecycleEventArgs $args)
     {
@@ -97,29 +100,122 @@ class SluggableSubscriber implements EventSubscriber
 
             if ($propertyMetadata instanceof PropertyMetadata && count($propertyMetadata->slugFields)) {
 
-                $values = [];
+                // try to generate custom slug
+                $slug = $this->generateSlugFromField($args, $propertyMetadata, $object);
 
-                foreach ($propertyMetadata->slugFields as $field) {
-
-                    if (!isset($classMetadata->propertyMetadata[$field])) {
-                        throw new MappingException(sprintf('Field "%s" does not exist.', $field));
-                    }
-
-                    $metadata = $classMetadata->propertyMetadata[$field];
-
-                    if ($metadata instanceof \Metadata\PropertyMetadata) {
-                        // get sluggable value
-                        $values[] = $metadata->getValue($object);
-                    }
+                if (!trim($slug)) {
+                    // generate slug from the sluggable fields
+                    $slug = $this->generateSlugFromMetadata($classMetadata, $propertyMetadata->slugFields, $object);
                 }
 
-                // generate slug value
-                $slug = $this->slugger->slugify(implode(' ', $values));
+                // generate unique slug
+                $slug = $this->generateSlugFromSimilar($om, get_class($object), $propertyMetadata->name, $slug);
 
                 $propertyMetadata->setValue($object, $slug);
                 $this->recomputeSingleObjectChangeSet($om, $object);
             }
         }
+    }
+
+    /**
+     * @param LifecycleEventArgs|ODMPreUpdateEventArgs|ORMPreUpdateEventArgs $args
+     * @param PropertyMetadata                                               $propertyMetadata
+     * @param object                                                         $object
+     *
+     * @return string|null
+     */
+    protected function generateSlugFromField(LifecycleEventArgs $args, $propertyMetadata, $object)
+    {
+        if ($args instanceof ODMPreUpdateEventArgs || $args instanceof ORMPreUpdateEventArgs) {
+
+            if ($args->hasChangedField($propertyMetadata->name)) {
+                // generate slug value
+                return $this->slugger->slugify($args->getNewValue($propertyMetadata->name));
+            }
+
+            return $propertyMetadata->getValue($object);
+        }
+    }
+
+    /**
+     * @param MergeableClassMetadata $classMetadata
+     * @param array                  $fields
+     * @param object                 $object
+     *
+     * @return string
+     *
+     * @throws MappingException
+     */
+    protected function generateSlugFromMetadata(MergeableClassMetadata $classMetadata, array $fields, $object)
+    {
+        $values = [];
+
+        foreach ($fields as $field) {
+
+            if (!isset($classMetadata->propertyMetadata[$field])) {
+                throw new MappingException(sprintf('Field "%s" does not exist.', $field));
+            }
+
+            $metadata = $classMetadata->propertyMetadata[$field];
+
+            if ($metadata instanceof \Metadata\PropertyMetadata) {
+                // get sluggable value
+                $values[] = $metadata->getValue($object);
+            }
+        }
+
+        // generate slug value
+        return $this->slugger->slugify(implode(' ', $values));
+    }
+
+    /**
+     * @param ObjectManager|DocumentManager|EntityManager $om
+     * @param string                                      $class
+     * @param string                                      $field
+     * @param string                                      $slug
+     *
+     * @return string
+     */
+    protected function generateSlugFromSimilar(ObjectManager $om, $class, $field, $slug)
+    {
+        $uow = $om->getUnitOfWork();
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        $objects = [];
+
+        if ($uow instanceof ODMUnitOfWork) {
+
+            $objects = $om->getRepository($class)->findBy([
+                $field => new \MongoRegex('/^' . preg_quote($slug, '/') . '(-\d+)?$/i')
+            ]);
+
+        } elseif ($uow instanceof ORMUnitOfWork) {
+
+            throw new \RuntimeException('Not implemented yet'); // @todo
+        }
+
+        if (count($objects)) {
+
+            $slugs = [];
+
+            foreach ($objects as $object) {
+
+                $value = $accessor->getValue($object, $field);
+                $index = (int) str_replace($slug, 1, str_replace($slug . '-', '', $value));
+
+                $slugs[$index] = $value;
+            }
+
+            for ($i = 1; $i <= (max(array_keys($slugs)) + 1); $i++) {
+
+                if (!isset($slugs[$i])) {
+                    // first available slug
+                    return $slug . ($i > 1 ? '-' . $i : '');
+                }
+            }
+        }
+
+        return $slug;
     }
 
     /**
@@ -136,7 +232,7 @@ class SluggableSubscriber implements EventSubscriber
             if ($uow instanceof ODMUnitOfWork) {
                 $uow->recomputeSingleDocumentChangeSet($classMetadata, $object);
 
-            } else if ($uow instanceof ORMUnitOfWork) {
+            } elseif ($uow instanceof ORMUnitOfWork) {
                 $uow->recomputeSingleEntityChangeSet($classMetadata, $object);
             }
         }
