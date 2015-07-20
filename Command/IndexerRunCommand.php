5c0704bc50b4ffda26b7e495db488b41dcbd466d
Marijn Otte <marijn@e-active.nl>
2015-07-20 11:11:43 +0200

    INTEGRATED-80 Convert files from tabs to spaces

--- Command/IndexerRunCommand.php
+++ Command/IndexerRunCommand.php
@@ -27,81 +27,81 @@ use Integrated\Common\Solr\Indexer\IndexerInterface;
  */
 class IndexerRunCommand extends ContainerAwareCommand
 {
-	/**
-	 * @see Command
-	 */
-	protected function configure()
-	{
-		$this
-			->setName('solr:indexer:run')
-
-			->addOption('full', 'f',InputOption::VALUE_NONE, 'Keep running until the queue is empty')
-			->addOption('daemon', 'd', InputOption::VALUE_NONE, 'Keep running until the programme is manually closed, this option overwrites --full')
-			->addOption('wait', 'w', InputOption::VALUE_REQUIRED, 'Time in milliseconds to wait between runs (in combination with --full or --daemon)', 0)
-
-			->setDescription('Execute a sol indexer run')
-			->setHelp('
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('solr:indexer:run')
+            ->addOption('full', 'f', InputOption::VALUE_NONE, 'Keep running until the queue is empty')
+            ->addOption('daemon', 'd', InputOption::VALUE_NONE, 'Keep running until the programme is manually closed, this option overwrites --full')
+            ->addOption('wait', 'w', InputOption::VALUE_REQUIRED, 'Time in milliseconds to wait between runs (in combination with --full or --daemon)', 0)
+            ->setDescription('Execute a sol indexer run')
+            ->setHelp('
 The <info>%command.name%</info> command starts a indexer run.
 
 <info>php %command.full_name%</info>
 '
-		);
-	}
+            );
+    }
 
-	/**
-	 * @see Command::execute()
-	 */
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		if ($input->getOption('full') || $input->getOption('daemon')) {
-			return $this->runExternal($input, $output);
-		}
+    /**
+     * @see Command::execute()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('full') || $input->getOption('daemon')) {
+            return $this->runExternal($input, $output);
+        }
 
-		return $this->runInternal($input, $output);
-	}
+        return $this->runInternal($input, $output);
+    }
 
-	private function runInternal(InputInterface $input, OutputInterface $output)
-	{
-		try	{
+    private function runInternal(InputInterface $input, OutputInterface $output)
+    {
+        try {
 
             $lock = new LockHandler('Integrated\Bundle\SolrBundle\Command\IndexerRunCommand');
             if (!$lock->lock()) {
                 throw new Exception('Indexer already running');
             }
 
-			/** @var IndexerInterface $indexer */
-			$indexer = $this->getContainer()->get('integrated_solr.indexer');
-			$indexer->execute();
+            /** @var IndexerInterface $indexer */
+            $indexer = $this->getContainer()->get('integrated_solr.indexer');
+            $indexer->execute();
 
-		} catch (Exception $e) {
-			$output->writeln("Aborting: " . $e->getMessage());
+        } catch (Exception $e) {
+            $output->writeln("Aborting: " . $e->getMessage());
 
-			return 1;
-		}
+            return 1;
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	private function runExternal(InputInterface $input, OutputInterface $output)
-	{
-		$wait = (int) $input->getOption('wait');
-		$wait = $wait * 1000; // convert from milli to micro
+    private function runExternal(InputInterface $input, OutputInterface $output)
+    {
+        $wait = (int)$input->getOption('wait');
+        $wait = $wait * 1000; // convert from milli to micro
 
-		while (true) {
-			$process = new Process('php app/console solr:indexer:run -e ' . $input->getOption('env'), getcwd(), null, null, null);
-			$process->run();
+        while (true) {
+            $process = new Process('php app/console solr:indexer:run -e ' . $input->getOption('env'), getcwd(), null, null, null);
+            $process->run();
 
-			if (!$process->isSuccessful()) {
-				break; // terminate when there is a error
-			}
+            if (!$process->isSuccessful()) {
+                break; // terminate when there is a error
+            }
 
-			if (!$input->getOption('daemon')) {
-				if (!$this->getContainer()->get('integrated_solr.indexer')->getQueue()->count()) { break; }
-			}
+            if (!$input->getOption('daemon')) {
+                if (!$this->getContainer()->get('integrated_solr.indexer')->getQueue()->count()) {
+                    break;
+                }
+            }
 
-			usleep($wait);
-		}
+            usleep($wait);
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 }
