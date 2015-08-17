4bc4be43898d267b61f77ce7e30acd46a352ab25
Johnny Borg <johnny@e-active.nl>
2015-08-17 15:38:32 +0200

    Added the ODM to the use imports.

--- Document/Image.php
+++ Document/Image.php
@@ -2,6 +2,8 @@
 
 namespace Integrated\Bundle\StorageBundle\Document;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
 /**
  * All data which might be required can be fetched from the upper class.
  * This empty class is for concrete implementations in the future.
