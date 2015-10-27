f910544fb590ee473b40ac1a4147af11bbd9aa8c
Vasil Pascal <developer.optimum@gmail.com>
2015-10-27 13:23:15 +0200

    Allow custom styles to be used in tinyMCE

---
+++ DependencyInjection/Compiler/RegisterTinymceParametersPass.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\FormTypeBundle\DependencyInjection\Compiler;
+
+use ReflectionClass;
+
+use Symfony\Component\Config\Resource\FileResource;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+/**
+ * @author Vasil Pascal <developer.optimum@gmail.com>
+ */
+class RegisterTinymceParametersPass implements CompilerPassInterface
+{
+    const STYLE_FORMAT = "style_formats";
+    const CONTENT_CSS = "content_css";
+    const PARAMETER_NAME = "integrated_tinymce_content_styles";
+
+    /** @var array */
+    private $parameters;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $this->parameters = [self::CONTENT_CSS => [], self::STYLE_FORMAT => []];
+
+        foreach ($container->getParameter('kernel.bundles') as $name => $class) {
+            $this->addParameters(dirname((new ReflectionClass($class))->getFileName()));
+        }
+
+        $container->getParameterBag()->add([self::PARAMETER_NAME => $this->parameters]);
+    }
+
+
+    /**
+     * @param $dir
+     * @return null
+     * @throws FileException
+     */
+    private function addParameters($dir)
+    {
+        $filePath = $dir.'/Resources/config/tinymce/contentstyle.xml';
+        if (!is_file($filePath)) {
+            return null;
+        }
+
+        $content = file_get_contents($filePath);
+        $crawler = new Crawler($content);
+        $options = $crawler->filter('styles')->children();
+
+        foreach ($options as $option) {
+            /** @var $option \DOMElement */
+            $type = $option->getAttribute('type');
+            $value = $option->nodeValue;
+
+            if (!in_array($type, [self::STYLE_FORMAT, self::CONTENT_CSS])) {
+                throw new FileException("The file $filePath is not valid");
+            }
+
+            $this->parameters[$type][] = $value;
+        }
+    }
+}
