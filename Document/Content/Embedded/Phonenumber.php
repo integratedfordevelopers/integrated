cc91ab269e11388efc9a204f94ca4773d19d8b5e
Jeroen van Leeuwen <jeroen@e-active.nl>
2015-10-30 12:09:02 +0100

    PSR-2 changes for Embedded Phonenumber

--- Document/Content/Embedded/Phonenumber.php
+++ Document/Content/Embedded/Phonenumber.php
@@ -18,50 +18,49 @@ namespace Integrated\Bundle\ContentBundle\Document\Content\Embedded;
  */
 class Phonenumber
 {
-	/**
-	 * @var string
-	 */
-	protected $type;
+    /**
+     * @var string
+     */
+    protected $type;
 
-	/**
-	 * @var string
-	 */
+    /**
+     * @var string
+     */
     protected $number;
 
-	/**
-	 * @param string $number
-	 * @return $this
-	 */
-	public function setNumber($number)
-	{
-		$this->number = $number === null ? null : (string) $number;
-		return $this;
-	}
+    /**
+     * @param string $number
+     * @return $this
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number === null ? null : (string) $number;
+        return $this;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getNumber()
-	{
-		return $this->number;
-	}
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
 
-	/**
-	 * @param string $type
-	 * @return $this
-	 */
-	public function setType($type)
-	{
-		$this->type = $type === null ? null : (string) $type;
-		return $this;
-	}
+    /**
+     * @param string $type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type === null ? null : (string) $type;
+        return $this;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getType()
-	{
-		return $this->type;
-	}
-
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 }
