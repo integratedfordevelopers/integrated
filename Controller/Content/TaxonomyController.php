e9becd61039e252674e59fd8308b1d0ab0ee5a87
Ger Jan <gerjan@e-active.nl>
2015-11-13 16:54:10 +0100

    [INTEGRATED-571] Added very basic content controllers (detail pages)

---
+++ Controller/Content/TaxonomyController.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\WebsiteBundle\Controller\Content;
+
+use Symfony\Bundle\TwigBundle\TwigEngine;
+
+use Integrated\Bundle\ThemeBundle\Templating\ThemeManager;
+use Integrated\Bundle\ContentBundle\Document\Content\Taxonomy;
+
+/**
+ * @author Ger Jan van den Bosch <gerjan@e-active.nl>
+ */
+class TaxonomyController
+{
+    /**
+     * @var TwigEngine
+     */
+    protected $templating;
+
+    /**
+     * @var ThemeManager
+     */
+    protected $themeManager;
+
+    /**
+     * @param TwigEngine $templating
+     * @param ThemeManager $themeManager
+     */
+    public function __construct(TwigEngine $templating, ThemeManager $themeManager)
+    {
+        $this->templating = $templating;
+        $this->themeManager = $themeManager;
+    }
+
+    /**
+     * @param Taxonomy $taxonomy
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showAction(Taxonomy $taxonomy)
+    {
+        return $this->templating->renderResponse($this->themeManager->locateTemplate('content/Taxonomy/default.html.twig'), [
+            'taxonomy' => $taxonomy,
+        ]);
+    }
+}
