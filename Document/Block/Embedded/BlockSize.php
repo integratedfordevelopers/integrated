7b26670b08c39d7a45fc9e5a01c89164307067a2
Johan Liefers <johan@e-active.nl>
2015-11-16 11:06:10 +0100

    [INTEGRATED-573] support for multiple bootstrap sizes in container block

--- Document/Block/Embedded/BlockSize.php
+++ Document/Block/Embedded/BlockSize.php
@@ -35,6 +35,30 @@ class BlockSize
     protected $size;
 
     /**
+     * @var int
+     * @ODM\Int
+     */
+    protected $sizeXs;
+
+    /**
+     * @var int
+     * @ODM\Int
+     */
+    protected $sizeSm;
+
+    /**
+     * @var int
+     * @ODM\Int
+     */
+    protected $sizeMd;
+
+    /**
+     * @var int
+     * @ODM\Int
+     */
+    protected $sizeLg;
+
+    /**
      * @return Block
      */
     public function getBlock()
@@ -54,6 +78,7 @@ class BlockSize
 
     /**
      * @return int
+     * @deprecated use sizeXs, sizeMd etc instead
      */
     public function getSize()
     {
@@ -62,11 +87,109 @@ class BlockSize
 
     /**
      * @param int $size
+     * @deprecated use sizeXs, sizeMd etc instead
      * @return $this
      */
     public function setSize($size)
     {
         $this->size = (int) $size;
         return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSizeXs()
+    {
+        return $this->sizeXs;
+    }
+
+    /**
+     * @param int $sizeXs
+     * @return $this
+     */
+    public function setSizeXs($sizeXs)
+    {
+        $this->sizeXs = $sizeXs;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSizeSm()
+    {
+        return $this->sizeSm;
+    }
+
+    /**
+     * @param int $sizeSm
+     * @return $this
+     */
+    public function setSizeSm($sizeSm)
+    {
+        $this->sizeSm = $sizeSm;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSizeMd()
+    {
+        return $this->sizeMd;
+    }
+
+    /**
+     * @param int $sizeMd
+     * @return $this
+     */
+    public function setSizeMd($sizeMd)
+    {
+        $this->sizeMd = $sizeMd;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSizeLg()
+    {
+        return $this->sizeLg;
+    }
+
+    /**
+     * @param int $sizeLg
+     * @return $this
+     */
+    public function setSizeLg($sizeLg)
+    {
+        $this->sizeLg = $sizeLg;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSizes()
+    {
+        $sizes = [];
+        if ($this->getSizeXs()) {
+            $sizes['xs'] = $this->getSizeXs();
+        } if ($this->getSizeSm()) {
+            $sizes['sm'] = $this->getSizeSm();
+        } if ($this->getSizeMd()) {
+            $sizes['md'] = $this->getSizeMd();
+        } if ($this->getSizeLg()) {
+            $sizes['lg'] = $this->getSizeLg();
+        }
+
+        if (!count($sizes)) {
+            if ($this->getSize()) {
+                $sizes['sm'] = $this->getSize();
+            }
+        }
+
+        return $sizes;
     }
 }
