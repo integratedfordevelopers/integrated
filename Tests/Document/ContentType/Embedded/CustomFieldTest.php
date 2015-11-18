71309a6d186bee0c115ad5ac4de86b704f0f15a9
Jeroen van Leeuwen <jeroen@e-active.nl>
2015-11-18 12:18:36 +0100

    Added test for CustomField

---
+++ Tests/Document/ContentType/Embedded/CustomFieldTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Tests\MongoDB\ContentType\Document\Embedded;
+
+use Integrated\Bundle\ContentBundle\Document\ContentType\Embedded\CustomField;
+
+/**
+ * @author Jeroen van Leeuwen <jeroen@e-active.nl>
+ */
+class CustomFieldTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CustomField
+     */
+    private $field;
+
+    /**
+     * Setup the test
+     */
+    protected function setUp()
+    {
+        $this->field = new CustomField();
+    }
+
+    /**
+     * Test instanceof
+     */
+    public function testInstanceOf()
+    {
+        return $this->assertInstanceOf('Integrated\Bundle\ContentBundle\Document\ContentType\Embedded\Field', $this->field);
+    }
+}
