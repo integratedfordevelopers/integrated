da974452a21e655b58626a9ed7635914bfd5d7d3
vasil <developer.optimum@gmail.com>
2015-12-09 11:22:55 +0200

    [INTEGRATED-277] - Related content block

---
+++ Block/RelatedContentBlockHandler.php
@@ -0,0 +1,157 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\ContentBundle\Block;
+
+use Doctrine\ODM\MongoDB\DocumentNotFoundException;
+
+//use MongoDBODMProxies\__CG__\Integrated\Bundle\ContentBundle\Document\Block\RelatedContentBlock;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
+
+use Integrated\Bundle\BlockBundle\Block\BlockHandler;
+use Integrated\Common\Block\BlockInterface;
+use Integrated\Bundle\ContentBundle\Document\Block\RelatedContentBlock;
+use Integrated\Bundle\ContentBundle\Provider\SolariumProvider;
+use Integrated\Bundle\ContentBundle\Document\Content\Article;
+use Doctrine\ODM\MongoDB\DocumentManager;
+
+/**
+ * Related content block handler
+ *
+ * @author Vasil <developer.optimum@gmail.com>
+ */
+class RelatedContentBlockHandler extends BlockHandler
+{
+    /**
+     * @var SolariumProvider
+     */
+    private $provider;
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * @var DocumentManager
+     */
+    private $dm;
+
+    /**
+     * @param SolariumProvider $provider
+     * @param RequestStack $requestStack
+     */
+    public function __construct(SolariumProvider $provider, RequestStack $requestStack, DocumentManager $dm)
+    {
+        $this->provider = $provider;
+        $this->requestStack = $requestStack;
+        $this->dm = $dm;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(BlockInterface $block)
+    {
+        if (!$block instanceof RelatedContentBlock) {
+            return;
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+
+        if (!$request instanceof Request) {
+            return;
+        }
+
+        $pagination = $this->getPagination($block, $request);
+
+        if (!count($pagination)) {
+            return;
+        }
+
+        return $this->render([
+            'block'      => $block,
+            'pagination' => $pagination,
+        ]);
+    }
+
+    /**
+     * @param RelatedContentBlock $block
+     * @param Request $request
+     * @param bool $exclude
+     * @return \Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination
+     */
+    public function getPagination(RelatedContentBlock $block, Request $request, $exclude = true)
+    {
+        $request = $request->duplicate(); // don't change original request
+
+        try {
+            /** @var Article $document */
+            $document = $this->getDocument();
+
+            if (!$document instanceof Article) {
+                return;
+            }
+
+            $relationType = $block->getRelation()->getType();
+            $articleId = $document->getId();
+
+            if ($block->getTypeBlock() == RelatedContentBlock::TYPE_BLOCK_2) {
+                $references = $document->getReferencesByRelationType($relationType);
+
+                if (count($references) > 0) {
+                    /** @var Article $firstReference */
+                    $firstReference = array_shift($references);
+
+                    $articleId = $firstReference->getId();
+                } else {
+                    return;
+                }
+            }
+
+            $repository = $this->dm->getRepository('IntegratedContentBundle:Content\Article');
+
+            $references = $repository->findBy(
+                array(
+                    'relations.relationType' => $relationType,
+                    'relations.references.$id'=>$articleId,
+                )
+            );
+
+            $related_articles = [];
+
+            /** @var Article $reference */
+            foreach ($references as $reference) {
+                $related_articles[] = $reference->getContentType().'-'.$reference->getId();
+            }
+
+            if (count($related_articles) == 0) {
+                return;
+            }
+
+            $request->query->add(['id'=>$related_articles]);
+        } catch (DocumentNotFoundException $e) {
+            // search selection is removed
+        }
+
+        $request->query->set('sort', $block->getSortBy());
+
+        return $this->provider->execute(
+            $request,
+            $block->getId(),
+            $block->getItemsPerPage(),
+            $block->getMaxItems(),
+            ['id'],
+            $exclude
+        );
+    }
+}
---
+++ Document/Block/RelatedContentBlock.php
@@ -0,0 +1,235 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\ContentBundle\Document\Block;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+use Integrated\Common\Form\Mapping\Annotations as Type;
+use Integrated\Bundle\BlockBundle\Document\Block\Block;
+
+/**
+ * Related content block document
+ *
+ * @author Vasil <developer.optimum@gmail.com>
+ *
+ * @ODM\Document
+ * @Type\Document("Related Content block")
+ */
+class RelatedContentBlock extends Block
+{
+
+    /**
+     * Show items which have the current document linked
+     */
+    const TYPE_BLOCK_1 = 1;
+
+    /**
+     * Show items which share linked items with the current document
+     */
+    const TYPE_BLOCK_2 = 2;
+
+    /**
+     * @var string
+     * @ODM\String
+     * @Assert\NotBlank
+     * @Type\Field
+     */
+    protected $published_title;
+
+    /**
+     * @var int
+     * @ODM\Int
+     * @Assert\NotBlank
+     * @Type\Field(
+     *     type="choice",
+     *     options={
+     *         "choices"={
+     *             1="Show items which have the current document linked",
+     *             2="Show items which share linked items with the current document",
+     *          },
+     *          "choices_as_values" = false,
+     *     }
+     * )
+     */
+    protected $type_block;
+
+    /**
+     * @var ContentBlock
+     * @ODM\ReferenceOne(targetDocument="Integrated\Bundle\ContentBundle\Document\Relation\Relation")
+     * @Type\Field(
+     *      type="document",
+     *      options={
+     *          "class"="IntegratedContentBundle:Relation\Relation",
+     *          "property"="name",
+     *          "placeholder"=""
+     *      }
+     * )
+     */
+    protected $relation;
+
+    /**
+     * @var int
+     * @ODM\Int
+     * @Assert\NotBlank
+     * @Type\Field(
+     *     type="choice",
+     *     options={
+     *         "choices"={
+     *             "time"="Publication date"
+     *          },
+     *          "choices_as_values" = false,
+     *     }
+     * )
+     */
+    protected $sort_by;
+
+    /**
+     * @var int
+     * @ODM\Int
+     * @Assert\Length(min=0)
+     * @Type\Field(
+     *      type="integer",
+     *      options={
+     *          "attr"={
+     *              "min"=0
+     *          }
+     *      }
+     * )
+     */
+    protected $itemsPerPage = 10;
+
+    /**
+     * @var int
+     * @ODM\Int
+     * @Assert\Length(min=0)
+     * @Type\Field(
+     *      type="integer",
+     *      options={
+     *          "required"=false,
+     *          "attr"={
+     *              "min"=0,
+     *          }
+     *      }
+     * )
+     */
+    protected $maxItems;
+
+    /**
+     * Get the block type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return 'related_content';
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublishedTitle()
+    {
+        return $this->published_title;
+    }
+
+    /**
+     * @param string $published_title
+     */
+    public function setPublishedTitle($published_title)
+    {
+        $this->published_title = $published_title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTypeBlock()
+    {
+        return $this->type_block;
+    }
+
+    /**
+     * @param string $type_block
+     */
+    public function setTypeBlock($type_block)
+    {
+        $this->type_block = $type_block;
+    }
+
+    /**
+     * @return ContentBlock
+     */
+    public function getRelation()
+    {
+        return $this->relation;
+    }
+
+    /**
+     * @param ContentBlock $relation
+     */
+    public function setRelation($relation)
+    {
+        $this->relation = $relation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSortBy()
+    {
+        return $this->sort_by;
+    }
+
+    /**
+     * @param string $sort_by
+     */
+    public function setSortBy($sort_by)
+    {
+        $this->sort_by = $sort_by;
+    }
+
+    /**
+     * @return int
+     */
+    public function getItemsPerPage()
+    {
+        return $this->itemsPerPage;
+    }
+
+    /**
+     * @param int $itemsPerPage
+     */
+    public function setItemsPerPage($itemsPerPage)
+    {
+        $this->itemsPerPage = $itemsPerPage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxItems()
+    {
+        return $this->maxItems;
+    }
+
+    /**
+     * @param int $maxItems
+     */
+    public function setMaxItems($maxItems)
+    {
+        $this->maxItems = $maxItems;
+    }
+
+
+}
