676bb0fafedb3f19b9df4cb041d3af503bc0463e
Johan Liefers <johan@e-active.nl>
2015-12-29 16:24:41 +0100

    moved workflow event and added own dispatcher

---
+++ src/Integrated/Common/Event/Workflow/WorkflowEvents.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Common\Event\Workflow;
+
+/**
+ * @author Johan Liefers <johan@e-active.nl>
+ */
+final class WorkflowEvents
+{
+    const STATE_CHANGED = 'integrated.workflow_state.changed';
+}
---
+++ src/Integrated/Common/Event/Workflow/WorkflowStateChangedEvent.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Common\Event\Workflow;
+
+use Symfony\Component\EventDispatcher\Event;
+
+use Integrated\Bundle\WorkflowBundle\Entity\Workflow\State;
+use Integrated\Common\Content\ContentInterface;
+
+/**
+ * @author Johan Liefers <johan@e-active.nl>
+ */
+class WorkflowStateChangedEvent extends Event
+{
+    /**
+     * @var State
+     */
+    protected $state;
+
+    /**
+     * @var ContentInterface
+     */
+    protected $content;
+
+    /**
+     * @param State $state
+     * @param ContentInterface $content
+     */
+    public function __construct(State $state, ContentInterface $content)
+    {
+        $this->state = $state;
+        $this->content = $content;
+    }
+
+    /**
+     * @return State
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @return ContentInterface
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
