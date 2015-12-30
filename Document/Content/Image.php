af4fd42b63775a703d0a6a9b76092ad220962199
Johnny Borg <johnny@e-active.nl>
2015-12-30 11:18:31 +0100

    Added the image class again.

---
+++ Document/Content/Image.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\ContentBundle\Document\Content;
+
+use Integrated\Common\Form\Mapping\Annotations as Type;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * Document type Image
+ *
+ * @author Johnny Borg <johnny@e-active.nl>
+ *
+ * @ODM\Document
+ * @Type\Document("Image")
+ */
+class Image extends File
+{
+
+}
