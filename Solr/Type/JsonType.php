4f6e22790fcd21d36c857f45cd37178c200f8d5e
Johan Liefers <johan@e-active.nl>
2016-01-07 15:03:20 +0100

    added jsontype

---
+++ Solr/Type/JsonType.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\SolrBundle\Solr\Type;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+use Integrated\Bundle\ContentBundle\Document\Content\Content;
+use Integrated\Common\Converter\ContainerInterface;
+use Integrated\Common\Converter\Type\TypeInterface;
+
+/**
+ * @author Johan Liefers <johan@e-active.nl>
+ */
+class JsonType implements TypeInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerInterface $container, $data, array $options = [])
+    {
+        if (!($data instanceof Content)) {
+            return;
+        }
+        $relations = $data->getReferencesByRelationId($options['relation_id']);
+
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        $array = [];
+        $i = 0;
+        foreach ($relations as $relation) {
+            foreach ($options['properties'] as $alias => $property) {
+                $array[$i][$alias] = $accessor->getValue($relation, $property);
+            }
+            $i++;
+        }
+
+        $container->set(
+            $options['alias'],
+            json_encode($array)
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'integrated.json';
+    }
+}
