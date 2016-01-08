becbde7a12c1f3035d4488d98d8f1cc5807fbbb3
Johan Liefers <johan@e-active.nl>
2016-01-08 16:58:56 +0100

    create content type events and event listener

---
+++ src/Integrated/Common/ContentType/Events.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Common\ContentType;
+
+/**
+ * @author Johan Liefers <johan@e-active.nl>
+ */
+final class Events
+{
+    const CONTENT_TYPE_CREATED = 'integrated.content_type.created';
+
+    const CONTENT_TYPE_CHANGED = 'integrated.content_type.changed';
+}
