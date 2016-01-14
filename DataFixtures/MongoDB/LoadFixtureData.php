4ec1fedd5c85a104a2572719ff1d97df1b98cf9a
Johnny Borg <johnny@e-active.nl>
2016-01-14 13:08:08 +0100

    Added data fixtures.

---
+++ DataFixtures/MongoDB/LoadFixtureData.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\StorageBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+use Integrated\Bundle\ContentBundle\Document\Content\Embedded\Storage;
+
+use Integrated\Bundle\StorageBundle\DataFixtures\MongoDB\Faker\StorageTrait;
+use Nelmio\Alice\Fixtures;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * @author Johnny Borg <johnny@e-active.nl>
+ */
+class LoadFixtureData extends ContainerAware implements FixtureInterface
+{
+    use StorageTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $files = array();
+        $finder =
+            Finder::create()
+                ->in(__DIR__ . DIRECTORY_SEPARATOR . 'alice')
+                ->name('*.yml')
+            ->sort(
+                function (SplFileInfo $a, SplFileInfo $b) {
+                    return (intval($a->getFilename()) < intval($b->getFilename()) ? -1 : 1);
+                }
+            )
+        ;
+
+        /** @var \Symfony\Component\Finder\SplFileInfo $file */
+        foreach ($finder as $file) {
+            $files[] = $file->getRealpath();
+        }
+
+        Fixtures::load($files, $manager, ['providers' => [$this]]);
+    }
+}
