e27016fb201e1bb38503781bc545186dc59c3c69
Johnny Borg <johnny@e-active.nl>
2016-03-18 15:06:05 +0100

    Moved the stuff around a bit, removed unneeded use statements and clarified phpdocs

--- src/Integrated/Common/Storage/ManagerInterface.php
+++ src/Integrated/Common/Storage/ManagerInterface.php
@@ -4,11 +4,9 @@ namespace Integrated\Common\Storage;
 
 use Integrated\Common\Content\Document\Storage\Embedded\StorageInterface;
 use Integrated\Common\Storage\Command\CommandInterface;
-use Integrated\Common\Storage\Handler\QueuedCommandBusInterface;
 use Integrated\Common\Storage\Reader\ReaderInterface;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Psr\Log\LoggerInterface;
 
 /**
  * @author Johnny Borg <johnny@e-active.nl>
@@ -37,18 +35,13 @@ interface ManagerInterface
     public function read(StorageInterface $storage);
 
     /**
+     * Write the file in the storage, all filesystems or specified
      * @param ReaderInterface $reader
      * @param ArrayCollection $filesystems
-     * @return StorageInterface
      * @throws \Exception
+     * @return StorageInterface
      */
     public function write(ReaderInterface $reader, ArrayCollection $filesystems = null);
-
-    /**
-     * @param StorageInterface $storage
-     * @return void
-     */
-    public function delete(StorageInterface $storage);
 
     /**
      * Move the file to the specified filesystems
@@ -58,4 +51,12 @@ interface ManagerInterface
      * @return StorageInterface
      */
     public function move(StorageInterface $storage, ArrayCollection $filesystems);
+
+    /**
+     * Delete the file in all known filesystems
+     *
+     * @param StorageInterface $storage
+     * @return void
+     */
+    public function delete(StorageInterface $storage);
 }
