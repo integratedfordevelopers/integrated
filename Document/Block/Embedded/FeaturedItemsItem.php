21629ad09526ecd584695e6f72fa8944d2aab3f7
Johan Liefers <johan@e-active.nl>
2016-06-17 11:39:29 +0200

    added featureditems block

---
+++ Document/Block/Embedded/FeaturedItemsItem.php
@@ -0,0 +1,216 @@
+<?php
+
+/*
+* This file is part of the Integrated package.
+*
+* (c) e-Active B.V. <integrated@e-active.nl>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Integrated\Bundle\BlockBundle\Document\Block\Embedded;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+use Integrated\Common\Content\Document\Storage\Embedded\StorageInterface;
+use Integrated\Common\Form\Mapping\Annotations as Type;
+
+/**
+ * @author Johan Liefers <johan@e-active.nl>
+ *
+ * @ODM\EmbeddedDocument
+ * @Type\Document("FeaturedItemsItem")
+ */
+class FeaturedItemsItem
+{
+    /**
+     * @var string
+     * @ODM\String
+     * @Type\Field
+     */
+    protected $title;
+
+    /**
+     * @var StorageInterface
+     * @ODM\EmbedOne(targetDocument="Integrated\Bundle\ContentBundle\Document\Content\Embedded\Storage")
+     * @Type\Field(type="integrated_image")
+     */
+    protected $image;
+
+    /**
+     * @var string
+     * @ODM\String
+     * @Type\Field
+     */
+    protected $link;
+
+    /**
+     * @var string
+     * @ODM\String
+     * @Type\Field(
+     *      type = "choice",
+     *      options = {
+     *          "label" = "Link target",
+     *          "expanded" = true,
+     *          "choices" = {
+     *               "_self"  = "Current window",
+     *               "_blank" = "New window"
+     *          }
+     *      }
+     *     )
+     */
+    protected $target = '_self';
+
+    /**
+     * @var string
+     * @ODM\String
+     * @Type\Field(type="textarea")
+     */
+    protected $text;
+
+    /**
+     * @var bool
+     * @ODM\Boolean
+     * @Type\Field(
+     *      type="checkbox",
+     *      options={
+     *          "required"=false
+     *      }
+     * )
+     */
+    protected $active = true;
+
+    /**
+     * @var int
+     * @ODM\Float
+     * @Type\Field(type="hidden", options={"attr"={"data-itemorder"="collection"}})
+     */
+    protected $order;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return StorageInterface
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param StorageInterface $image
+     * @return $this
+     */
+    public function setImage(StorageInterface $image = null)
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * @param string $link
+     * @return $this
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * @param string $target
+     * @return $this
+     */
+    public function setTarget($target)
+    {
+        $this->target = $target;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param boolean $active
+     * @return $this
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param int $order
+     * @return $this
+     */
+    public function setOrder($order)
+    {
+        $this->order = (int) $order;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     * @return $this
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+}
---
+++ Document/Block/FeaturedItemsBlock.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\BlockBundle\Document\Block;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+use Integrated\Common\Form\Mapping\Annotations as Type;
+
+/**
+ * @author Johan Liefers <johan@e-active.nl>
+ *
+ * @ODM\Document
+ * @Type\Document("Feature item block")
+ */
+class FeaturedItemsBlock extends Block
+{
+    use PublishTitleTrait;
+    
+    /**
+     * @var ArrayCollection
+     * @ODM\EmbedMany(targetDocument="Integrated\Bundle\BlockBundle\Document\Block\Embedded\FeaturedItemsItem")
+     * @Type\Field(
+     *      type="integrated_sortable_collection",
+     *      options={
+     *          "type"="integrated_embedded_document",
+     *          "options"={
+     *              "data_class"="Integrated\Bundle\BlockBundle\Document\Block\Embedded\FeaturedItemsItem"
+     *          },
+     *          "allow_add"=true,
+     *          "allow_delete"=true
+     *      }
+     * )
+     */
+    protected $items;
+
+    /**
+     * General object init
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->items = new ArrayCollection();
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems()
+    {
+        $items = $this->items->toArray();
+
+        usort($items, function ($a, $b) {
+            return $a->getOrder() - $b->getOrder();
+        });
+
+        return $items;
+    }
+
+    /**
+     * @param ArrayCollection $items
+     * @return $this
+     */
+    public function setItems($items)
+    {
+        $this->items = $items;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return 'featured_items';
+    }
+}
