de9f166d3f99f152d9854eee764842ff00a8e97d
Johnny Borg <johnny@e-active.nl>
2016-07-15 09:47:04 +0200

    Added Relation extension

---
+++ DataFixtures/MongoDB/Extension/RelationExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\ContentBundle\DataFixtures\MongoDB\Extension;
+
+use Integrated\Bundle\ContentBundle\Document\Content\Relation\Relation;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @author Johnny Borg <johnny@e-active.nl>
+ */
+trait RelationExtension
+{
+    /**
+     * @return ContainerInterface
+     */
+    abstract public function getContainer();
+
+    /**
+     * @param string $id
+     * @return null|Relation
+     */
+    public function contentType($id)
+    {
+        return $this->getContainer()
+            ->get('doctrine.odm.mongodb.document_manager')
+            ->getRepository(Relation::class)->find($id)
+        ;
+    }
+}
