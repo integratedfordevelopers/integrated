9261217f5216b1acad171f4e61335537491e76ad
Jan Sanne <jansanne@e-active.nl>
2016-09-15 14:41:10 +0200

    Added a task to queue all the content connected to a content type when the said content type is changed.

---
+++ tests/Integrated/Tests/Common/Solr/Task/Tasks/Doctrine/EventListener/MongoDBContenTypeListenerTest.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Tests\Common\Solr\Task\Tasks\Doctrine\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
+use Doctrine\ODM\MongoDB\Events;
+
+use Integrated\Common\ContentType\ContentTypeInterface;
+use Integrated\Common\Queue\QueueInterface;
+use Integrated\Common\Solr\Task\Tasks\Doctrine\EventListener\MongoDBContentTypeListener;
+use Integrated\Common\Solr\Task\Tasks\ContentTypeQueueTask;
+
+use stdClass;
+
+/**
+ * @author Jan Sanne Mulder <jansanne@e-active.nl>
+ */
+class MongoDBContentTypeListenerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var QueueInterface | \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $queue;
+
+    protected function setUp()
+    {
+        $this->queue = $this->getMock(QueueInterface::class);
+    }
+
+    public function testInterface()
+    {
+        $this->assertInstanceOf(EventSubscriber::class, $this->getInstance());
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        self::assertEquals([
+            Events::postUpdate,
+        ], $this->getInstance()->getSubscribedEvents());
+    }
+
+    public function testPostUpdate()
+    {
+        $this->queue->expects($this->once())
+            ->method('push')
+            ->with($this->callback(function (ContentTypeQueueTask $task) {
+                self::assertEquals('this-is-the-id', $task->getId());
+
+                return true;
+            }));
+
+        $this->getInstance()->postUpdate($this->getEvent($this->getContentType('this-is-the-id')));
+    }
+
+    public function testPostUpdateNoContentType()
+    {
+        $this->queue->expects($this->never())
+            ->method($this->anything());
+
+        $this->getInstance()->postUpdate($this->getEvent(new stdClass()));
+    }
+
+
+    /**
+     * @return MongoDBContentTypeListener
+     */
+    protected function getInstance()
+    {
+        return new MongoDBContentTypeListener($this->queue);
+    }
+
+    /**
+     * @param string $id
+     * @return ContentTypeInterface | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getContentType($id)
+    {
+        $mock = $this->getMock(ContentTypeInterface::class);
+        $mock->expects($this->atLeastOnce())
+            ->method('getId')
+            ->willReturn($id);
+
+        return $mock;
+    }
+
+    /**
+     * @param object $document
+     * @return LifecycleEventArgs | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getEvent($document)
+    {
+        $mock = $this->getMockBuilder(LifecycleEventArgs::class)->disableOriginalConstructor()->getMock();
+        $mock->expects($this->atLeastOnce())
+            ->method('getDocument')
+            ->willReturn($document);
+
+        return $mock;
+    }
+}
