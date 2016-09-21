4e306265df8afb895babcf54596d62bea188bfce
Johnny Borg <johnny@e-active.nl>
2016-09-21 13:16:29 +0200

    Partially rebuild of the editor which now can be used with any content type.

--- src/Integrated/Common/Content/ContentInterface.php
+++ src/Integrated/Common/Content/ContentInterface.php
@@ -11,6 +11,11 @@
 
 namespace Integrated\Common\Content;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+use Integrated\Bundle\ContentBundle\Document\Content\Embedded\Relation;
+
 /**
  * @author Jeroen van Leeuwen <jeroen@e-active.nl>
  * @author Jan Sanne Mulder <jansanne@e-active.nl>
@@ -38,4 +43,41 @@ interface ContentInterface
      * @return $this
      */
     public function setContentType($contentType);
+
+    /**
+     * Get the relations of the document
+     *
+     * @return ArrayCollection
+     */
+    public function getRelations();
+
+    /**
+     * @param $relationId
+     * @return Relation|false
+     */
+    public function getRelation($relationId);
+
+    /**
+     * Set the relations of the document
+     *
+     * @param Collection $relations
+     * @return $this
+     */
+    public function setRelations(Collection $relations);
+
+    /**
+     * Add relation to relations collection
+     *
+     * @param Relation $relation
+     * @return $this
+     */
+    public function addRelation(Relation $relation);
+
+    /**
+     * Remove relation from relations collection
+     *
+     * @param Relation $relation
+     * @return $this
+     */
+    public function removeRelation(Relation $relation);
 }
