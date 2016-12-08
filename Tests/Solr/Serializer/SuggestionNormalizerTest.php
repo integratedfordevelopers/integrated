a10437801109620a4d3d0e6911fd4a3d4d41eaa0
Jeroen van Leeuwen <joenivl@gmail.com>
2016-12-08 12:35:55 +0100

    Cleaned up the test

--- Tests/Solr/Serializer/SuggestionNormalizerTest.php
+++ Tests/Solr/Serializer/SuggestionNormalizerTest.php
@@ -8,22 +8,21 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Integrated\Bundle\ContentBundle\Tests\Solr\Serializer;
 
-use Integrated\Bundle\ContentBundle\Document\Content\Content;
 use Integrated\Bundle\ContentBundle\Solr\Query\SuggestionQuery;
-use Solarium\Core\Client\Client;
-use Solarium\Core\Client\Response;
-use Solarium\Core\Query\AbstractQuery;
+use Integrated\Bundle\ContentBundle\Solr\Serializer\SuggestionNormalizer;
+use Integrated\Common\ContentType\ContentTypeInterface;
+use Integrated\Common\ContentType\ResolverInterface;
+
 use Solarium\QueryType\Select\Query\Component\FacetSet;
+use Solarium\QueryType\Select\Result\Document;
 use Solarium\QueryType\Select\Result\Result;
 
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Exception\InvalidArgumentException;
-
-use Integrated\Bundle\ContentBundle\Solr\Serializer\SuggestionNormalizer;
-use Integrated\Common\ContentType\ResolverInterface;
 
 /**
  * @author Michael Jongman <michael@e-active.nl>
@@ -31,57 +30,73 @@ use Integrated\Common\ContentType\ResolverInterface;
 class SuggestionNormalizerTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var UrlGeneratorInterface | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $generator;
+
+    /**
+     * @var ResolverInterface | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $resolver;
+
+    /**
+     * @var Result | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $result;
+
+    /**
+     * @var string
+     */
+    protected $route = 'test-route';
+
+    /**
      * Set up
      */
     public function setUp()
     {
         $this->generator = $this->getMock(UrlGeneratorInterface::class);
         $this->resolver = $this->getMock(ResolverInterface::class);
-    }
-
-    public function testInterface()
-    {
-        self::assertInstanceOf(NormalizerInterface::class, $this->getSuggestionNormalizer());
+        $this->result = $this->getMockBuilder(Result::class)->disableOriginalConstructor()->getMock();
     }
 
     /**
-     * @expectedException InvalidArgumentException
-     *
+     * Normalizer must be an instance of the NormalizerInterface
+     */
+    public function testInterface()
+    {
+        self::assertInstanceOf(NormalizerInterface::class, $this->getInstance());
+    }
+
+    /**
      * Testing if the first parameter is an instanceof Result
+     * @expectedException InvalidArgumentException
      */
     public function testNormalizerWithInvalidObject()
     {
-        $this->getSuggestionNormalizer()->normalize(
-            'test'
-        );
+        $this->getInstance()->normalize('test');
     }
 
     /**
-     * @expectedException InvalidArgumentException
-     *
      * Testing if the Result query function is an instanceof SuggestionQuery
+     * @expectedException InvalidArgumentException
      */
     public function testNormalizerWithInvalidQuery()
     {
-        $object = $this->getSolrResult();
-
-        $object
+        $this->result
             ->expects($this->once())
             ->method('getQuery')
             ->willReturn(null)
         ;
 
-        $this->getSuggestionNormalizer()->normalize(
-            $object
-        );
+        $this->getInstance()->normalize($this->result);
     }
 
+    /**
+     * Test the Normalizer with empty data
+     */
     public function testNormalizerWithEmptyData()
     {
-        $object = $this->getSolrResult();
-
         $query = $this->getMock(SuggestionQuery::class);
-
         $facetSet = $this->getMock(FacetSet::class);
 
         $facetSet
@@ -91,140 +106,158 @@ class SuggestionNormalizerTest extends \PHPUnit_Framework_TestCase
             ->willReturn([])
         ;
 
-        $object
+        $this->result
             ->expects($this->once())
             ->method('getFacetSet')
             ->willReturn($facetSet)
         ;
 
-        $object
+        $this->result
             ->expects($this->once())
             ->method('getDocuments')
             ->willReturn([])
         ;
 
-        $object
+        $this->result
             ->expects($this->exactly(2))
             ->method('getQuery')
             ->willReturn($query)
         ;
 
-        $this->assertEquals([], $this->getSuggestionNormalizer()->normalize(
-            $object
-        ));
+        $this->assertEquals([], $this->getInstance()->normalize($this->result));
     }
 
-    public function testNormalizer()
+    /**
+     * Test the Normalizer with data
+     */
+    public function testNormalizerWithData()
     {
-        $object = $this->getSolrResult();
-
         $query = $this->getMock(SuggestionQuery::class);
-
         $facetSet = $this->getMock(FacetSet::class);
+
+        $suggestions = [
+            'suggestion' => 5,
+            'suggest' => 0,
+            'suggestions' => 4
+        ];
 
         $facetSet
             ->expects($this->once())
             ->method('getFacet')
             ->with('suggest')
-            ->willReturn([
-                'suggestion' => 5,
-                'suggest' => 2,
-                'suggestions' => 4
-            ])
+            ->willReturn($suggestions)
         ;
 
-        $object
+        $this->result
             ->expects($this->exactly(2))
             ->method('getQuery')
             ->willReturn($query)
         ;
 
-        $object
+        $this->result
             ->expects($this->once())
             ->method('getFacetSet')
             ->willReturn($facetSet)
         ;
 
-        $object
-            ->expects($this->once())
-            ->method('getDocuments')
-            ->willReturn($this->getDocuments())
+        $documents = [
+            new Document(
+                [
+                    'type_id' => 'id_0',
+                    'type_name' => 'news',
+                    'title' => 'title_0',
+                    'pub_time' => 'Invalid timestamp',
+                    'pub_edited' => '2015-01-01 08:00'
+                ]
+            ),
+            new Document(
+                [
+                    'type_id' => 'id_1',
+                    'type_name' => 'convert_to_news',
+                    'title' => 'title_1',
+                    'pub_time' => '2015-01-01',
+                ]
+            )
+        ];
+
+        $this->resolver
+            ->expects($this->exactly(2))
+            ->method('hasType')
+            ->withConsecutive(
+                [$this->equalTo('news')],
+                [$this->equalTo('convert_to_news')]
+            )
+            ->willReturnOnConsecutiveCalls(
+                false,
+                true
+            )
         ;
 
-        $documents = $this->getDocuments();
+        $contentType = $this->getMock(ContentTypeInterface::class);
 
-        $this->assertEquals($documents[0]['type_id'], 'type');
-        $this->assertEquals($documents[1]['title'], 'title document 2');
+        $this->resolver
+            ->expects($this->once())
+            ->method('getType')
+            ->with($this->equalTo('convert_to_news'))
+            ->willReturn($contentType)
+        ;
+
+        $contentType
+            ->expects($this->once())
+            ->method('getName')
+            ->willReturn('news')
+        ;
+
+        $this->generator
+            ->expects($this->exactly(2))
+            ->method('generate')
+            ->withConsecutive(
+                [$this->route, ['id' => 'id_0']],
+                [$this->route, ['id' => 'id_1']]
+            )
+            ->willReturnOnConsecutiveCalls(
+                'url_0',
+                'url_1'
+            )
+        ;
+
+        $this->result
+            ->expects($this->once())
+            ->method('getDocuments')
+            ->willReturn($documents)
+        ;
 
         $this->assertEquals(
             [
-                'suggestions' => [
-                    'suggestion',
-                    'suggest',
-                    'suggestions'
-                ],
+                'suggestions' => array_keys($suggestions),
                 'results' => [
-                    0 => [
-                        'id' => 'type',
-                        'title' => 'title document',
+                    [
+                        'id' => 'id_0',
+                        'type' => 'news',
+                        'title' => 'title_0',
+                        'url' => 'url_0',
+                        'published' => null,
+                        'updated' => '2015-01-01 08:00'
                     ],
-                    1 => [
-                        'id' => 'type2',
-                        'title' => 'title document 2'
-                    ]
+                    [
+                        'id' => 'id_1',
+                        'type' => 'news',
+                        'title' => 'title_1',
+                        'url' => 'url_1',
+                        'published' => '2015-01-01',
+                        'updated' => null
+                    ],
                 ]
             ],
-            $this->getSuggestionNormalizer()->normalize(
-                $object
-            )
+            $this->getInstance()->normalize($this->result)
         );
     }
 
     /**
      * @return SuggestionNormalizer
      */
-    protected function getSuggestionNormalizer()
+    protected function getInstance()
     {
-        return new SuggestionNormalizer($this->generator, 'test-route', $this->resolver);
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function getSolrResult()
-    {
-        return $this->getMock(
-            Result::class,
-            [],
-            [
-                Client::class,
-                AbstractQuery::class,
-                $this->getMock(
-                    Response::class,
-                    [],
-                    [
-                        '',
-                        ''
-                    ]
-                )
-            ]
-        );
-    }
-
-    /**
-     * @return array
-     */
-    protected function getDocuments()
-    {
-        return [
-            [
-                'title' => 'title document',
-                'type_id' => 'type'
-            ],
-            [
-                'title' => 'title document 2',
-                'type_id' => 'type2'
-            ]
-        ];
+        return new SuggestionNormalizer($this->generator, $this->route, $this->resolver);
     }
 }
