f721a8c6ec7b0e890260843ad10457485986139a
Johan Liefers <johan@e-active.nl>
2017-01-19 14:28:27 +0100

    [INTEGRATED-960] comment bundle fixes

--- Controller/ContentController.php
+++ Controller/ContentController.php
@@ -1066,7 +1066,7 @@ class ContentController extends Controller
                         ['id' => $content->getId()]
             ),
             'method' => 'PUT',
-            'attr' => ['class' => 'content-form'],
+            'attr' => ['class' => 'content-form', 'data-content-id' => $content->getId()],
             // don't display error's when the content is locked as the user can't save in the first place
             'validation_groups' => $locking['locked'] ? false : null
         ]);
