4351e19c48e1663930111baa1cd7b6b95eb5a54f
Jeroen van Leeuwen <jeroen@e-active.nl>
2017-01-20 11:21:00 +0100

    Fixed the ActionsType

--- Form/Type/ActionsType.php
+++ Form/Type/ActionsType.php
@@ -24,7 +24,7 @@ class ActionsType extends BaseActionsType
      */
     public function __construct()
     {
-        parent::__construct('integrated_content_actions', [
+        parent::__construct(self::class, [
             'create'         => ['type' => SubmitType::class, 'options' => ['label' => 'Create', 'button_class' => 'orange']],
             'save'           => ['type' => SubmitType::class, 'options' => ['label' => 'Save', 'button_class' => 'orange']],
             'delete'         => ['type' => SubmitType::class, 'options' => ['label' => 'Delete', 'button_class' => 'orange']],
@@ -33,5 +33,13 @@ class ActionsType extends BaseActionsType
             'reload_changed' => ['type' => SubmitType::class, 'options' => ['label' => 'Reload (keep changes)', 'attr' => ['formnovalidate' => 'formnovalidate']]],
             'cancel'         => ['type' => SubmitType::class, 'options' => ['label' => 'Cancel', 'button_class' => 'gray-thin', 'attr' => ['formnovalidate' => 'formnovalidate', 'data-dismiss' => 'modal']]],
         ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'integrated_content_actions';
     }
 }
