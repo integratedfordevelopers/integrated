36effb6661f3fb9cddfbb97a17f4be57e5d127e2
Jan Sanne <jansanne@e-active.nl>
2017-01-23 13:11:08 +0100

    Added a color picker form type.

---
+++ Form/DataTransformer/ColorTransformer.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\FormTypeBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+/**
+ * @author Jan Sanne Mulder <jansanne@e-active.nl>
+ */
+class ColorTransformer implements DataTransformerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function transform($value)
+    {
+        $value = strtolower(trim($value));
+
+        if ($value && $value[0] === '#') {
+            $value = substr($value, 1);
+        }
+
+        if (strlen($value) == 6 && ctype_xdigit($value)) {
+            return '#' . $value;
+        }
+
+        return '';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function reverseTransform($value)
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        if (!is_string($value)) {
+            throw new TransformationFailedException('Expected a string.');
+        }
+
+        $value = strtolower(trim($value));
+
+        if (!$value) {
+            return '';
+        }
+
+        if (strlen($value) == 7 && $value[0] === '#' && ctype_xdigit(substr($value, 1))) {
+            return $value;
+        }
+
+        throw new TransformationFailedException(sprintf('The value %s is not a valid hexadecimal color string.', $value));
+    }
+}
---
+++ Form/Type/ColorType.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\FormTypeBundle\Form\Type;
+
+use Integrated\Bundle\AssetBundle\Manager\AssetManager;
+
+use Integrated\Bundle\FormTypeBundle\Form\DataTransformer\ColorTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Jan Sanne Mulder <jansanne@e-active.nl>
+ */
+class ColorType extends AbstractType
+{
+    /**
+     * @var AssetManager
+     */
+    private $manager;
+
+    /**
+     * @param AssetManager $manager
+     */
+    public function __construct(AssetManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->manager->add('bundles/integratedformtype/components/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
+
+        $builder->addViewTransformer(new ColorTransformer());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'integrated_color';
+    }
+}
