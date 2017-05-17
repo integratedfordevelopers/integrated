6d558784aa7f435cbd8031215abefff5264d8c97
Michael Jongman <michael@e-active.nl>
2017-05-17 10:31:11 +0200

    Added a check on the integrated scope when trying to login

--- Doctrine/UserManager.php
+++ Doctrine/UserManager.php
@@ -14,6 +14,7 @@ namespace Integrated\Bundle\UserBundle\Doctrine;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
 
+use Integrated\Bundle\UserBundle\Model\ScopeInterface;
 use Integrated\Bundle\UserBundle\Model\UserInterface;
 use Integrated\Bundle\UserBundle\Model\UserManagerInterface;
 
@@ -24,148 +25,170 @@ use InvalidArgumentException;
  */
 class UserManager implements UserManagerInterface
 {
-	/**
-	 * @var ObjectManager
-	 */
-	private $om;
+    /**
+     * @var ObjectManager
+     */
+    private $om;
 
-	/**
-	 * @var ObjectRepository
-	 */
-	private $repository;
+    /**
+     * @var ObjectRepository
+     */
+    private $repository;
 
-	public function __construct(ObjectManager $om, $class)
-	{
-		$this->om = $om;
-		$this->repository = $this->om->getRepository($class);
+    /**
+     * @param ObjectManager $om
+     * @param $class
+     */
+    public function __construct(ObjectManager $om, $class)
+    {
+        $this->om = $om;
+        $this->repository = $this->om->getRepository($class);
 
-		if (!is_subclass_of($this->repository->getClassName(), 'Integrated\\Bundle\\UserBundle\\Model\\UserInterface')) {
-			throw new InvalidArgumentException(sprintf('The class "%s" is not subclass of Integrated\\Bundle\\UserBundle\\Model\\UserInterface', $this->repository->getClassName()));
-		}
-	}
+        if (!is_subclass_of($this->repository->getClassName(), 'Integrated\\Bundle\\UserBundle\\Model\\UserInterface')) {
+            throw new InvalidArgumentException(sprintf('The class "%s" is not subclass of Integrated\\Bundle\\UserBundle\\Model\\UserInterface', $this->repository->getClassName()));
+        }
+    }
 
-	/**
-	 * @return ObjectManager
-	 */
-	public function getObjectManager()
-	{
-		return $this->om;
-	}
+    /**
+     * @return ObjectManager
+     */
+    public function getObjectManager()
+    {
+        return $this->om;
+    }
 
-	/**
-	 * @return ObjectRepository
-	 */
-	public function getRepository()
-	{
-		return $this->repository;
-	}
+    /**
+     * @return ObjectRepository
+     */
+    public function getRepository()
+    {
+        return $this->repository;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function create()
-	{
-		$class = $this->getClassName();
-		return new $class();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function create()
+    {
+        $class = $this->getClassName();
+        return new $class();
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function persist(UserInterface $user, $flush = true)
-	{
-		$this->om->persist($user);
+    /**
+     * {@inheritdoc}
+     */
+    public function persist(UserInterface $user, $flush = true)
+    {
+        $this->om->persist($user);
 
-		if ($flush) {
-			$this->om->flush($user);
-		}
-	}
+        if ($flush) {
+            $this->om->flush($user);
+        }
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function remove(UserInterface $user, $flush = true)
-	{
-		$this->om->remove($user);
+    /**
+     * {@inheritdoc}
+     */
+    public function remove(UserInterface $user, $flush = true)
+    {
+        $this->om->remove($user);
 
-		if ($flush) {
-			$this->om->flush($user);
-		}
-	}
+        if ($flush) {
+            $this->om->flush($user);
+        }
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function clear()
-	{
-		$this->om->clear($this->repository->getClassName());
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function clear()
+    {
+        $this->om->clear($this->repository->getClassName());
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function find($id)
-	{
-		return $this->repository->find($id);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function find($id)
+    {
+        return $this->repository->find($id);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function findAll()
-	{
-		return $this->repository->findAll();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function findAll()
+    {
+        return $this->repository->findAll();
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function findByUsername($criteria)
-	{
-		return $this->repository->findOneBy(['username' => $criteria]);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function findByUsername($criteria)
+    {
+        return $this->repository->findOneBy(['username' => $criteria]);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function findByEmail($criteria)
-	{
-		return $this->repository->findOneBy(['email' => $criteria]);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function findByEmail($criteria)
+    {
+        return $this->repository->findOneBy(['email' => $criteria]);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function findByUsernameOrEmail($criteria)
-	{
-		if ($user = $this->findByUsername($criteria)) {
-			return $user;
-		}
+    /**
+     * {@inheritdoc}
+     */
+    public function findByUsernameOrEmail($criteria)
+    {
+        if ($user = $this->findByUsername($criteria)) {
+            return $user;
+        }
 
-		return $this->findByEmail($criteria);
-	}
+        return $this->findByEmail($criteria);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
-	{
-		return $this->repository->findBy($criteria, $orderBy, $limit, $offset);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        return $this->repository->findBy($criteria, $orderBy, $limit, $offset);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getClassName()
-	{
-		return $this->repository->getClassName();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getClassName()
+    {
+        return $this->repository->getClassName();
+    }
 
-	/**
-	 * @return \Doctrine\ORM\QueryBuilder
-	 */
-	public function createQueryBuilder()
-	{
-		return $this->repository->createQueryBuilder($this->getClassName());
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function findByUsernameAndScope($username, ScopeInterface $scope)
+    {
+        return $this->createQueryBuilder()
+            ->select('User')
+            ->leftJoin('User.scope', 'Scope')
+            ->where('User.username = :username')
+            ->andWhere('(Scope.admin = true OR User.scope = :scope)')
+            ->setParameters([
+                'username' => $username,
+                'scope' => (int) $scope->getId()
+            ])
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function createQueryBuilder()
+    {
+        return $this->repository->createQueryBuilder('User');
+    }
 }
--- Model/UserManagerInterface.php
+++ Model/UserManagerInterface.php
@@ -16,95 +16,97 @@ namespace Integrated\Bundle\UserBundle\Model;
  */
 interface UserManagerInterface extends ManagerInterface
 {
-	/**
-	 * Create a user object
-	 *
-	 * @return UserInterface
-	 */
-	public function create();
+    /**
+     * Create a user object
+     *
+     * @return UserInterface
+     */
+    public function create();
 
-	/**
-	 * Change or add the user to the manager
-	 *
-	 * @param UserInterface $user
-	 */
-	public function persist(UserInterface $user);
+    /**
+     * Change or add the user to the manager
+     *
+     * @param UserInterface $user
+     */
+    public function persist(UserInterface $user);
 
-	/**
-	 * Remove the user from the manager
-	 *
-	 * @param UserInterface $user
-	 */
-	public function remove(UserInterface $user);
+    /**
+     * Remove the user from the manager
+     *
+     * @param UserInterface $user
+     */
+    public function remove(UserInterface $user);
 
-	/**
-	 * Delete all the managed users.
-	 */
-	public function clear();
+    /**
+     * Delete all the managed users.
+     */
+    public function clear();
 
-//	/**
-//	 * Return the total number of users
-//	 *
-//	 * @return int
-//	 */
-//	public function count();
+    /**
+     * Finds the user by its identifier.
+     *
+     * @param mixed $id
+     *
+     * @return UserInterface
+     */
+    public function find($id);
 
-	/**
-	 * Finds the user by its identifier.
-	 *
-	 * @param mixed $id
-	 *
-	 * @return UserInterface
-	 */
-	public function find($id);
+    /**
+     * Finds all the managed users.
+     *
+     * @return UserInterface[]
+     */
+    public function findAll();
 
-	/**
-	 * Finds all the managed users.
-	 *
-	 * @return UserInterface[]
-	 */
-	public function findAll();
+    /**
+     * Finds the user by its username.
+     *
+     * @param string $criteria
+     *
+     * @return UserInterface
+     */
+    public function findByUsername($criteria);
 
-	/**
-	 * Finds the user by its username.
-	 *
-	 * @param string $criteria
-	 *
-	 * @return UserInterface
-	 */
-	public function findByUsername($criteria);
+    /**
+     * Finds the user by its email address
+     *
+     * @param string $criteria
+     *
+     * @return UserInterface
+     */
+    public function findByEmail($criteria);
 
-	/**
-	 * Finds the user by its email address
-	 *
-	 * @param string $criteria
-	 *
-	 * @return UserInterface
-	 */
-	public function findByEmail($criteria);
+    /**
+     * Finds the user by its username or email address.
+     *
+     * @param string $criteria
+     *
+     * @return UserInterface
+     */
+    public function findByUsernameOrEmail($criteria);
 
-	/**
-	 * Finds the user by its username or email address.
-	 *
-	 * @param string $criteria
-	 *
-	 * @return UserInterface
-	 */
-	public function findByUsernameOrEmail($criteria);
+    /**
+     * Finds the users by a set of criteria.
+     *
+     * @param array $criteria
+     *
+     * @return UserInterface[]
+     */
+    public function findBy(array $criteria);
 
-	/**
-	 * Finds the users by a set of criteria.
-	 *
-	 * @param array $criteria
-	 *
-	 * @return UserInterface[]
-	 */
-	public function findBy(array $criteria);
+    /**
+     * Returns the class name of the user object
+     *
+     * @return string
+     */
+    public function getClassName();
 
-	/**
-	 * Returns the class name of the user object
-	 *
-	 * @return string
-	 */
-	public function getClassName();
+    /**
+     * Finds the user by its username and scope
+     *
+     * @param $username
+     * @param ScopeInterface $scope
+     * @return UserInterface
+     */
+    public function findByUsernameAndScope($username, ScopeInterface $scope);
 }
