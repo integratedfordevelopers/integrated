a63d6286cc305b88644cb6afbf92af32ab6953a7
Johan Liefers <johan@e-active.nl>
2017-05-26 12:32:24 +0200

    [INTEGRATED-1115] Add textblock button

---
+++ EventListener/GridItemSubscriber.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\PageBundle\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\MongoDB\Events;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
+use Doctrine\ODM\MongoDB\Event\PreUpdateEventArgs;
+use Integrated\Bundle\BlockBundle\Document\Block\TextBlock;
+use Integrated\Bundle\PageBundle\Document\Page\Grid\Grid;
+use Integrated\Bundle\PageBundle\Document\Page\Grid\Item;
+use Integrated\Bundle\PageBundle\Document\Page\Grid\ItemsInterface;
+use Integrated\Bundle\PageBundle\Document\Page\Page;
+
+/**
+ * @author Johan Liefers <johan@e-active.nl>
+ */
+class GridItemSubscriber implements EventSubscriber
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::preRemove => 'preRemove',
+            Events::preUpdate => 'preUpdate',
+        ];
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function preRemove(LifecycleEventArgs $args)
+    {
+        $document = $args->getDocument();
+
+        if (!$document instanceof Page) {
+            return;
+        }
+
+        foreach ($this->getDbTextBlocks($args->getDocumentManager(), $document) as $block) {
+            $args->getDocumentManager()->remove($block);
+        }
+    }
+
+    /**
+     * @param PreUpdateEventArgs $args
+     */
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $document = $args->getDocument();
+
+        if (!$document instanceof Page) {
+            return;
+        }
+
+        $oldBlocks = $this->getDbTextBlocks($args->getDocumentManager(), $document);
+
+        $newBlocks = $this->getTextBlocks($document);
+
+        foreach (array_diff($oldBlocks, $newBlocks) as $removedBlock) {
+            $args->getDocumentManager()->remove($removedBlock);
+        }
+    }
+
+    protected function getDbTextBlocks(DocumentManager $dm, Page $page)
+    {
+        return $dm->getRepository(TextBlock::class)->findBy(['parentPage' => $page]);
+    }
+
+    /**
+     * @param Page $page
+     * @return array
+     */
+    protected function getTextBlocks(Page $page)
+    {
+        $blocks = [];
+
+        foreach ($page->getGrids() as $grid) {
+            if ($grid instanceof Grid) {
+                $blocks = array_merge($blocks, $this->getGridTextBlocks($grid, $page));
+            }
+        }
+
+        return $blocks;
+    }
+
+    /**
+     * @param ItemsInterface $grid
+     * @param Page $page
+     * @return array
+     */
+    protected function getGridTextBlocks(ItemsInterface $grid, Page $page)
+    {
+        $blocks = [];
+
+        foreach ($grid->getItems() as $item) {
+            if (!$item instanceof Item) {
+                continue;
+            }
+
+            $block = $item->getBlock();
+
+            if ($block instanceof TextBlock && $page == $block->getParentPage()) {
+                $blocks[$block->getId()] = $block;
+            }
+
+            if ($item->getRow()) {
+                foreach ($item->getRow()->getColumns() as $column) {
+                    $blocks = array_merge($blocks, $this->getGridTextBlocks($column, $page));
+                }
+            }
+        }
+
+        return $blocks;
+    }
+}
