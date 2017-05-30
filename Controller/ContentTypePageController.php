b43f6834a3a922221b34bcdaa3dc5fd86f20dcdc
Johan Liefers <johan@e-active.nl>
2017-05-30 09:57:44 +0200

    [INTEGRATED-1115] fix feedback

--- Controller/ContentTypePageController.php
+++ Controller/ContentTypePageController.php
@@ -71,7 +71,7 @@ class ContentTypePageController extends Controller
 
             $this->get('braincrafted_bootstrap.flash')->success('Page updated');
 
-            return $this->redirect($this->generateUrl('integrated_page_content_type_page_index', ['channel' => $channel->getId()]));
+            return $this->redirectToRoute('integrated_page_content_type_page_index', ['channel' => $channel->getId()]);
         }
 
         return [
