9bc96865d19fce7e800c0738bd1a643b32e19066
Johan Liefers <johan@e-active.nl>
2017-07-04 15:23:07 +0200

    [INTEGRATED-1174] automagically add integrated urls to solr

--- DependencyInjection/IntegratedWebsiteExtension.php
+++ DependencyInjection/IntegratedWebsiteExtension.php
@@ -37,5 +37,6 @@ class IntegratedWebsiteExtension extends Extension
         $loader->load('connector.xml');
         $loader->load('controllers.xml');
         $loader->load('twig.xml');
+        $loader->load('solr.xml');
     }
 }
