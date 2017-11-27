6c63c86a3a37273d7a57cc2ea3fcacfbedf732f8
Jan Sanne <jansanne@e-active.nl>
2017-11-27 13:53:43 +0100

    Formatted and added missing license headers.

--- src/Common/Storage/FilesystemRegistryInterface.php
+++ src/Common/Storage/FilesystemRegistryInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Integrated\Common\Storage;
 
 /**
