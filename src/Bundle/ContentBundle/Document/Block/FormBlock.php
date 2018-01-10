14987e21bdaaf8fc2c4288a16c40c06ba19fa2ff
Jeroen van Leeuwen <jeroen@e-active.nl>
2018-01-10 11:01:21 +0100

    Use full class name for form types

--- src/Bundle/ContentBundle/Document/Block/FormBlock.php
+++ src/Bundle/ContentBundle/Document/Block/FormBlock.php
@@ -42,7 +42,7 @@ class FormBlock extends Block
     /**
      * @var string
      * @Type\Field(
-     *     type="text",
+     *     type="Symfony\Component\Form\Extension\Core\Type\TextType",
      *     options={
      *          "required"=false,
      *     }
@@ -53,7 +53,7 @@ class FormBlock extends Block
     /**
      * @var string
      * @Type\Field(
-     *     type="textarea",
+     *     type="Symfony\Component\Form\Extension\Core\Type\TextareaType",
      *     options={
      *          "required"=false,
      *     }
@@ -82,7 +82,7 @@ class FormBlock extends Block
     /**
      * @var bool
      * @Type\Field(
-     *      type="checkbox",
+     *      type="Symfony\Component\Form\Extension\Core\Type\CheckboxType",
      *      options={
      *          "required"=false
      *      }
