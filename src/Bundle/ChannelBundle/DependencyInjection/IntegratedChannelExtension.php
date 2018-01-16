37e32f021b7497445ebe7762e760e36d8b5d0c16
Jan Sanne <jansanne@e-active.nl>
2018-01-16 13:59:53 +0100

    Update missed form theme path with new lowercase templating directory name.

--- src/Bundle/ChannelBundle/DependencyInjection/IntegratedChannelExtension.php
+++ src/Bundle/ChannelBundle/DependencyInjection/IntegratedChannelExtension.php
@@ -118,7 +118,7 @@ class IntegratedChannelExtension extends Extension implements PrependExtensionIn
                 case 'twig':
                     $container->prependExtensionConfig(
                         $name,
-                        ['form_themes' => ['IntegratedChannelBundle:Form:options.html.twig']]
+                        ['form_themes' => ['IntegratedChannelBundle:form:options.html.twig']]
                     );
                     break;
             }
