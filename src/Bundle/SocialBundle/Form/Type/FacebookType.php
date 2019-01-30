8033b7477efb47090baf032ea87601b14d7f10e6
Marijn Otte <marijn@e-active.nl>
2019-01-30 12:11:59 +0100

    Fetch page token after submit

--- src/Bundle/SocialBundle/Form/Type/FacebookType.php
+++ src/Bundle/SocialBundle/Form/Type/FacebookType.php
@@ -71,39 +71,10 @@ class FacebookType extends AbstractType
                         $formData['apiStatus'] = 'Your account does not seem to be administrator of a Facebook page';
                     } else {
                         $formData['apiStatus'] = 'OK';
+
+                        $form->add('page_token', TextType::class, ['attr' => ['readonly' => 'true']]);
                     }
 
-                    if (isset($formData['page']) && is_numeric($formData['page'])) {
-                        $pageToken = null;
-                        if ($formData['page_token']) {
-                            $response = $this->facebook->get(
-                                '/debug_token?input_token=' . $formData['page_token'],
-                                $formData['page_token'],
-                                null,
-                                'v3.2'
-                            );
-
-                            $data = $response->getDecodedBody();
-                            if ($data['data']["profile_id"] == $formData['page']) {
-                                $pageToken = $formData['page_token'];
-                            }
-                        }
-
-                        if ($pageToken) {
-                            $form->add('page_token', TextType::class, ['attr' => ['readonly' => 'true']]);
-                        } else {
-                            $response = $this->facebook->get(
-                                '/'.$formData['page'].'?fields=access_token',
-                                $formData['token'],
-                                null,
-                                'v3.2'
-                            );
-
-                            $data = $response->getDecodedBody();
-
-                            $form->add('page_token', TextType::class, ['attr' => ['readonly' => 'true'], 'data' => $data['access_token']]);
-                        }
-                    }
                 } catch (\Exception $e) {
                     $formData['token'] = null;
                     $formData['apiStatus'] = 'Token seems to be invalid. Save the form to get a new one. ('.$e->getMessage().')';
@@ -113,6 +84,43 @@ class FacebookType extends AbstractType
                 $formData['apiStatus'] = 'Save the configuration to connect to Facebook.';
             }
         });
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) use ($facebook) {
+            $formData = $event->getData();
+            if ($formData && $formData['token'] && $formData['page'] && is_numeric($formData['page'])) {
+                $pageToken = null;
+                if ($formData['page_token']) {
+                    $response = $this->facebook->get(
+                        '/debug_token?input_token=' . $formData['page_token'],
+                        $formData['page_token'],
+                        null,
+                        'v3.2'
+                    );
+
+                    $data = $response->getDecodedBody();
+                    if ($data['data']["profile_id"] == $formData['page']) {
+                        $pageToken = $formData['page_token'];
+                    }
+                }
+
+                if (!$pageToken) {
+                    $response = $this->facebook->get(
+                        '/'.$formData['page'].'?fields=access_token',
+                        $formData['token'],
+                        null,
+                        'v3.2'
+                    );
+
+                    $tokenData = $response->getDecodedBody();
+
+                    $data = $event->getData();
+                    $data['page_token'] = $tokenData['access_token'];
+                    $event->setData($data);
+                }
+
+            }
+        });
+
         $builder->add('token', TextType::class, ['attr' => ['readonly' => 'true']]);
         $builder->add('apiStatus', TextType::class, ['attr' => ['readonly' => 'true']]);
     }
