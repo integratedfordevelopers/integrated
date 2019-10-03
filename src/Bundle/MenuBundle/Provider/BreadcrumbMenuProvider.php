f6de51f6498b00867d428905930342f9b109483a
Marijn Otte <marijn@e-active.nl>
2019-10-03 10:40:24 +0200

    Add BreadcrumbResolver

--- src/Bundle/MenuBundle/Provider/BreadcrumbMenuProvider.php
+++ src/Bundle/MenuBundle/Provider/BreadcrumbMenuProvider.php
@@ -11,64 +11,30 @@
 
 namespace Integrated\Bundle\MenuBundle\Provider;
 
-use Doctrine\ODM\MongoDB\DocumentManager;
-use Integrated\Bundle\ContentBundle\Document\Content\Content;
-use Integrated\Bundle\PageBundle\Document\Page\Page;
-use Integrated\Bundle\PageBundle\Services\UrlResolver;
-use Integrated\Common\Content\Channel\ChannelContextInterface;
-use Integrated\Common\Content\Channel\ChannelInterface;
+use Integrated\Bundle\PageBundle\Breadcrumb\BreadcrumbResolver;
 use Knp\Menu\FactoryInterface;
-use Knp\Menu\ItemInterface;
 use Knp\Menu\Provider\MenuProviderInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 class BreadcrumbMenuProvider implements MenuProviderInterface
 {
     /**
      * @var FactoryInterface
      */
-    protected $factory = null;
+    protected $menuFactory = null;
 
     /**
-     * @var ChannelContextInterface
+     * @var BreadcrumbResolver
      */
-    protected $channelContext;
+    private $breadcrumbResolver;
 
     /**
-     * @var ItemInterface[]
+     * @param FactoryInterface   $menuFactory
+     * @param BreadcrumbResolver $breadcrumbResolver
      */
-    protected $menus = [];
-
-    /**
-     * @var Request|null
-     */
-    protected $request;
-
-    /**
-     * @var DocumentManager
-     */
-    private $documentManager;
-
-    /**
-     * @var UrlResolver
-     */
-    private $urlResolver;
-
-    /**
-     * @param FactoryInterface        $factory
-     * @param ChannelContextInterface $channelContext
-     * @param RequestStack            $requestStack
-     * @param DocumentManager         $documentManager
-     * @param UrlResolver             $urlResolver
-     */
-    public function __construct(FactoryInterface $factory, ChannelContextInterface $channelContext, RequestStack $requestStack, DocumentManager $documentManager, UrlResolver $urlResolver)
+    public function __construct(FactoryInterface $menuFactory, BreadcrumbResolver $breadcrumbResolver)
     {
-        $this->factory = $factory;
-        $this->channelContext = $channelContext;
-        $this->request = $requestStack->getMasterRequest();
-        $this->documentManager = $documentManager;
-        $this->urlResolver = $urlResolver;
+        $this->menuFactory = $menuFactory;
+        $this->breadcrumbResolver = $breadcrumbResolver;
     }
 
     /**
@@ -76,48 +42,16 @@ class BreadcrumbMenuProvider implements MenuProviderInterface
      */
     public function get($name, array $options = [])
     {
-        $channel = $this->channelContext->getChannel();
+        $menu = $this->menuFactory->createItem($name, $options);
 
-        if ($channel instanceof ChannelInterface) {
-            $channel = $channel->getId();
+        foreach ($this->breadcrumbResolver->getBreadCrumb() as $breadcrumbItem) {
+            $menu->addChild(
+                $breadcrumbItem->getName(),
+                ['uri' => $breadcrumbItem->getUrl()]
+            );
         }
 
-        if (isset($this->menus[$channel])) {
-            return $this->menus[$channel];
-        }
-
-        $url = $this->request->getPathInfo();
-        $menu = $this->factory->createItem($name, $options);
-
-        $parts = explode('/', $url);
-        $url = '';
-        foreach ($parts as $part) {
-            $url .= $part;
-            $page = $this->documentManager->getRepository(Page::class)->findOneBy(['path' => ($url == '') ? '/' : $url, 'channel.$id' => $channel]);
-
-            if ($page !== null) {
-                /* @var $page Page */
-                $menu->addChild(
-                    $page->getTitle(),
-                    ['uri' => ($url == '') ? '/' : $url]
-                );
-            } else {
-                $contentItem = $this->documentManager->getRepository(Content::class)->findOneBy(['slug' => $part, 'channels.$id' => $channel]);
-                if ($contentItem !== null && $contentItem->isPublished() && strpos($this->urlResolver->generateUrl($contentItem), $url) !== false) {
-                    /* @var $page Page */
-                    $menu->addChild(
-                        (string) $contentItem,
-                        ['uri' => $url]
-                    );
-                }
-            }
-
-            $url .= '/';
-        }
-
-        $this->menus[$channel] = $menu;
-
-        return $this->menus[$channel];
+        return $menu;
     }
 
     /**
---
+++ src/Bundle/PageBundle/Breadcrumb/BreadcrumbResolver.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of the Integrated package.
+ *
+ * (c) e-Active B.V. <integrated@e-active.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Integrated\Bundle\PageBundle\Breadcrumb;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Integrated\Bundle\ContentBundle\Document\Content\Content;
+use Integrated\Bundle\PageBundle\Document\Page\Page;
+use Integrated\Bundle\PageBundle\Services\UrlResolver;
+use Integrated\Common\Content\Channel\ChannelContextInterface;
+use Integrated\Common\Content\Channel\ChannelInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class BreadcrumbResolver
+{
+    /**
+     * @var DocumentManager
+     */
+    private $documentManager;
+
+    /**
+     * @var UrlResolver
+     */
+    private $urlResolver;
+
+    /**
+     * @var ChannelContextInterface
+     */
+    protected $channelContext;
+
+    /**
+     * @var BreadcrumbItem[]
+     */
+    protected $breadcrumb = null;
+
+    /**
+     * @var Request|null
+     */
+    protected $request;
+
+    /**
+     * @param ChannelContextInterface $channelContext
+     * @param RequestStack            $requestStack
+     * @param DocumentManager         $documentManager
+     * @param UrlResolver             $urlResolver
+     */
+    public function __construct(DocumentManager $documentManager, UrlResolver $urlResolver, ChannelContextInterface $channelContext, RequestStack $requestStack)
+    {
+        $this->documentManager = $documentManager;
+        $this->urlResolver = $urlResolver;
+        $this->channelContext = $channelContext;
+        $this->request = $requestStack->getMasterRequest();
+    }
+
+    public function getBreadCrumb()
+    {
+        if (isset($this->breadcrumb)) {
+            return $this->breadcrumb;
+        }
+
+        $channel = $this->channelContext->getChannel();
+
+        if (!$channel instanceof ChannelInterface) {
+            return [];
+        }
+
+        $channel = $channel->getId();
+
+        $url = $this->request->getPathInfo();
+
+        $parts = explode('/', $url);
+        $url = '';
+        foreach ($parts as $part) {
+            $url .= $part;
+
+            //support Page
+            if ($page = $this->documentManager->getRepository(Page::class)->findOneBy(['path' => ($url == '') ? '/' : $url, 'channel.$id' => $channel])) {
+                /* @var Page $page */
+                $this->breadcrumb[] = new BreadcrumbItem($page->getTitle(), ($url == '') ? '/' : $url);
+                $url .= '/';
+                continue;
+            }
+
+            //support Content
+            if ($content = $this->documentManager->getRepository(Content::class)->findOneBy(['slug' => $part, 'channels.$id' => $channel])) {
+                /* @var Content $content */
+                if ($content->isPublished() && strpos($this->urlResolver->generateUrl($content), $url) !== false) {
+                    $this->breadcrumb[] = new BreadcrumbItem((string) $content, $url);
+                }
+                $url .= '/';
+                continue;
+            }
+
+            $url .= '/';
+        }
+
+        return $this->breadcrumb;
+    }
+}
