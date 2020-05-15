e909fef411049506a6d3374223f1528aa1f5b91a
Marijn Otte <marijn@e-active.nl>
2020-05-15 11:41:29 +0200

    Add 0.11 migrations

--- src/Bundle/InstallerBundle/Migrations/MySQL/Version20200514145156.php
+++ src/Bundle/InstallerBundle/Migrations/MySQL/Version20200514145156.php
@@ -12,6 +12,7 @@ final class Version20200514145156 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE security_users ADD created_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE channel_connector_config ADD publication_start_date DATETIME NOT NULL;');
     }
 
     public function down(Schema $schema) : void
@@ -19,5 +20,6 @@ final class Version20200514145156 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE security_users DROP created_at');
+        $this->addSql('ALTER TABLE channel_connector_config DROP publication_start_date');
     }
 }
