954511431789eb192a020a089502bd846db68cbc
Marijn Otte <marijn@e-active.nl>
2020-06-22 11:16:30 +0200

    Fix install command

--- src/Bundle/InstallerBundle/Install/MySQLMigrations.php
+++ src/Bundle/InstallerBundle/Install/MySQLMigrations.php
@@ -3,7 +3,6 @@
 namespace Integrated\Bundle\InstallerBundle\Install;
 
 use Doctrine\Migrations\Configuration\Configuration;
-use Doctrine\Migrations\Exception\UnknownMigrationVersion;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
