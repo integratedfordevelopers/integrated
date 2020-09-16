9bb217d0771e6880bf702a246ff5f3fb942b394a
Marijn Otte <marijn@e-active.nl>
2020-09-16 13:08:37 +0200

    Fix code style

--- src/Bundle/InstallerBundle/Migrations/MySQL/Version20200916104700.php
+++ src/Bundle/InstallerBundle/Migrations/MySQL/Version20200916104700.php
@@ -9,19 +9,19 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20200916104700 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE INDEX IDX_F83F464362894749 ON security_users (relation)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
