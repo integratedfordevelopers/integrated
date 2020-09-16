10b391a0bcaa6a28c89c80f6364e70fefdb4b85a
Marijn Otte <marijn@e-active.nl>
2020-09-16 13:00:50 +0200

    Add index on users relation field

---
+++ src/Bundle/InstallerBundle/Migrations/MySQL/Version20200916104700.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integrated\Bundle\InstallerBundle\Migrations\MySQL;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200916104700 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE INDEX IDX_F83F464362894749 ON security_users (relation)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX idx_f83f464362894749 ON security_users');
+    }
+}
