487c273d71cbb11513b7996476f5bacca0650e36
Marijn Otte <marijn@e-active.nl>
2021-11-25 14:45:13 +0100

    Add extension and update service configuration

--- src/Bundle/FormTypeBundle/DependencyInjection/IntegratedFormTypeExtension.php
+++ src/Bundle/FormTypeBundle/DependencyInjection/IntegratedFormTypeExtension.php
@@ -28,6 +28,7 @@ class IntegratedFormTypeExtension extends Extension
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
+        $loader->load('extension.xml');
         $loader->load('event_listener.xml');
         $loader->load('services.xml');
         $loader->load('twig.xml');
