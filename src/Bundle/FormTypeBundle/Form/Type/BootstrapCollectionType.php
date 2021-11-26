d370111385b061b3e556e4abb045dc2e959ce196
Marijn Otte <marijn@e-active.nl>
2021-11-26 09:44:52 +0100

    Bugfixes and temporary stylesheet fix

--- src/Bundle/FormTypeBundle/Form/Type/BootstrapCollectionType.php
+++ src/Bundle/FormTypeBundle/Form/Type/BootstrapCollectionType.php
@@ -3,6 +3,7 @@
 namespace Integrated\Bundle\FormTypeBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
