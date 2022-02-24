5543e4058e2b30dfe5c73ad6f0ffe3ab4416b35d
koen <koen@e-active.nl>
2022-02-24 13:49:42 +0100

    INTEGRATED-1678 webpack js changes

---
+++ src/Bundle/ContentBundle/Resources/assets/js/main.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+global.$ = global.jQuery = $;
+
+import Handlebars from 'handlebars';
+global.Handlebars = Handlebars;
+
+import 'bootstrap-sass';
+import 'typeahead';
+import 'jquery-placeholder';
+import 'moment';
+import 'select2/dist/js/select2.full';
+
+import './scripts'
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 let Encore = require('@symfony/webpack-encore');
+const PathResolver = require("path");
 
-Encore.setOutputPath('./src/Bundle/IntegratedBundle/Resources/public')
+webpackConfig = Encore.setOutputPath('./src/Bundle/IntegratedBundle/Resources/public')
     .setPublicPath('/bundles/integratedintegrated')
     .setManifestKeyPrefix('bundles/integratedintegrated')
     .addEntry('app', [
         './src/Bundle/ContentBundle/Resources/assets/sass/main.scss',
-        './src/Bundle/CommentBundle/Resources/assets/css/comments.css',
-        './src/Bundle/StorageBundle/Resources/assets/css/drag-drop.css',
         './src/Bundle/WorkflowBundle/Resources/assets/css/style.css',
+        './src/Bundle/ContentBundle/Resources/assets/js/main.js',
     ])
     .addEntry('iframe', [
         './src/Bundle/BlockBundle/Resources/assets/css/iframe.css',
@@ -15,11 +15,49 @@ Encore.setOutputPath('./src/Bundle/IntegratedBundle/Resources/public')
     .addEntry('tinymce', [
         './src/Bundle/FormTypeBundle/Resources/assets/css/tinymce.content.css',
         './src/Bundle/FormTypeBundle/Resources/assets/css/tinymce.editor.css',
+        './node_modules/tinymce/jquery.tinymce.min.js',
+        './node_modules/tinymce/tinymce.min.js',
+    ])
+    .addEntry('content_edit', [
+        './node_modules/select2/dist/js/select2.full',
+        './src/Bundle/ContentBundle/Resources/assets/js/handlebars.helpers.js',
+        './src/Bundle/ContentBundle/Resources/assets/js/relation.js',
+        './src/Bundle/UserBundle/Resources/assets/js/visible_user_form.js',
+        './src/Bundle/WorkflowBundle/Resources/assets/js/workflowChangeState.js',
+    ])
+    .addEntry('content_sortable', [
+        './node_modules/components-jqueryui/jquery-ui.min.js',
+        './src/Bundle/FormTypeBundle/Resources/assets/js/content_sortable_collection.js',
+    ])
+    .addEntry('datetimepicker', [
+        './node_modules/jquery-datetimepicker/jquery.datetimepicker.css',
+        './node_modules/jquery-datetimepicker/build/jquery.datetimepicker.min.js',
+        './src/Bundle/FormTypeBundle/Resources/assets/js/datetimepicker.js',
+    ])
+    .addEntry('drag-drop', [
+        './node_modules/jquery.filer/css/jquery.filer.css',
+        './node_modules/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css',
+        './node_modules/jquery.filer/js/jquery.filer.js',
+        './src/Bundle/StorageBundle/Resources/assets/css/drag-drop.css',
+        './src/Bundle/StorageBundle/Resources/assets/js/drag-drop.js',
+    ])
+    .addEntry('workflow', [
+        './src/Bundle/WorkflowBundle/Resources/assets/js/updateTransitions.js',
+        './src/Bundle/WorkflowBundle/Resources/assets/js/defaultSelection.js',
+        './src/Bundle/WorkflowBundle/Resources/assets/js/select2_init.js',
     ])
     .cleanupOutputBeforeBuild()
+    .autoProvidejQuery()
     .enableSassLoader()
     .enableSourceMaps(false)
     .enableVersioning(false)
     .disableSingleRuntimeChunk()
+    .getWebpackConfig();
 
-module.exports = Encore.getWebpackConfig();
+webpackConfig.resolve.alias = {
+    typeahead: PathResolver.resolve(__dirname, "node_modules/typeahead.js/dist/typeahead.bundle.min.js")
+};
+
+webpackConfig.resolve.fallback = {'fs': false};
+
+module.exports = webpackConfig;
